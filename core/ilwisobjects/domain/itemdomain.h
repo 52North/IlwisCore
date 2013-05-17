26e44ecb08f96aa644d16d9eff3378e02c71d8f5
MSchouwenburg <m.schouwenburg@utwente.nl>
2013-05-17 14:38:10 +0200

    added comments

--- core/ilwisobjects/domain/itemdomain.h
+++ core/ilwisobjects/domain/itemdomain.h
@@ -9,7 +9,7 @@ namespace Ilwis {
 class ItemRange;
 
 /*!
-ItemDomains are domains that speak about discrete data. For example the domain of a thematic classification. There are various
+ItemDomains are domains that handle discrete data. For example the domain of a thematic classification. There are various
 item domains that are all defined by the item type they control. This is passed as template parameter to the domain.
 The range of the domain contains all the valid items for that domain.
  */
@@ -25,6 +25,11 @@ public:
         delete _range;
     }
 
+    /*!
+     returns a string representation of the item pointed to by the index
+     * \param 0 based index, if the index is invalid sUNDEF will be returned
+     * \return the string representation or sUNDEDF in case no items are defined
+     */
     QString value(double index) const {
         if (!_range) {
             ERROR1(ERR_NO_INITIALIZED_1, name());
@@ -32,6 +37,11 @@ public:
         }
        return _range->value(index);
     }
+    /*!
+     returns a pointer to the domain item pointed to by the index
+     * \param 0 based index, if the index is invalid 0 will be returned
+     * \return a (pointer to) domain item or 0 if no items are defined
+     */
     C* item(quint32 index) const {
         if (!_range) {
             ERROR1(ERR_NO_INITIALIZED_1, name());
@@ -39,7 +49,11 @@ public:
         }
         return static_cast<C *>(_range->item(index)) ;
     }
-
+    /*!
+     returns a pointer to the item that is identified by the string name
+     * \param a name that must be in the range of the domain. If not a 0 pointer will be returned
+     * \return a (pointer to) domain item or 0 if no items are defined
+     */
     C* item(const QString& nam) const{
         if (!_range) {
             ERROR1(ERR_NO_INITIALIZED_1, name());
@@ -47,13 +61,21 @@ public:
         }
         return _range->item(nam) ;
     }
+    /*!
+     Adds an item of the templated type to the range. If no range is yet defined, one will be created
+     * \param the item to be added. Note that ownership of the item is transferred to the range. no delete allowed
+     *
+     */
     void addItem(C* thing) {
         if ( !_range) {
             _range = C::createRange();
         }
        _range->add(thing);
     }
-
+    /*!
+     removes an item from the range
+     * \param the item to be removed
+     */
     void removeItem(const QString& nme){
         if (!_range) {
             ERROR1(ERR_NO_INITIALIZED_1, name());
@@ -65,7 +87,7 @@ public:
     void addRange(const ItemRange& range)
     {
         for(quint32 i=0; i < range.count(); ++i) {
-           addItem(range.item(i));
+            addItem(range.item(i));
         }
     }
     quint32 count() const {
