95f83c7b3ddd576740d26f5febda838f3a3c6e17
MSchouwenburg <m.schouwenburg@utwente.nl>
2013-05-24 13:19:58 +0200

    created; convenience compund h files

---
+++ core/ilwisobjects/coverage/raster.h
@@ -0,0 +1,20 @@
+#ifndef RASTER_H
+#define RASTER_H
+
+#include "kernel.h"
+#include "angle.h"
+#include "point.h"
+#include "box.h"
+#include "ilwisobject.h"
+#include "ilwisdata.h"
+#include "domain.h"
+#include "coordinatesystem.h"
+#include "valuedefiner.h"
+#include "connectorinterface.h"
+#include "containerstatistics.h"
+#include "coverage.h"
+#include "georeference.h"
+#include "grid.h"
+#include "gridcoverage.h"
+
+#endif // RASTER_H
---
+++ core/ilwisobjects/operation/ilwisoperation.h
@@ -0,0 +1,10 @@
+#ifndef ILWISOPERATION_H
+#define ILWISOPERATION_H
+
+#include "OperationExpression.h"
+#include "operationmetadata.h"
+#include "operation.h"
+#include "commandhandler.h"
+#include "operationhelper.h"
+
+#endif // ILWISOPERATION_H
