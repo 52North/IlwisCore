5aec825f8dd25ff48b012f6fd096cd37c8ca6b44
MSchouwenburg <m.schouwenburg@utwente.nl>
2013-05-24 13:20:13 +0200

    created

---
+++ baseoperations/data/iffraster.h
@@ -0,0 +1,21 @@
+#ifndef IFFRASTER_H
+#define IFFRASTER_H
+
+namespace Ilwis {
+namespace BaseOperations {
+class IffRaster : public OperationImplementation
+{
+public:
+    IffRaster();
+    IffRaster(quint64 metaid, const Ilwis::OperationExpression &expr);
+
+    bool execute(ExecutionContext *ctx=0);
+    static Ilwis::OperationImplementation *create(quint64 metaid,const Ilwis::OperationExpression& expr);
+    Ilwis::OperationImplementation::State prepare();
+
+    static quint64 createMetadata();
+};
+}
+}
+
+#endif // IFFRASTER_H
