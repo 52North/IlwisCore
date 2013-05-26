af65d5d47205970959f426a9b2b4dba21f8d7179
Martin Schouwenburg <mschouwen@gmail.com>
2013-05-26 09:13:54 +0200

    added few includes for multithreeaded support

--- core/ilwisobjects/operation/ilwisoperation.h
+++ core/ilwisobjects/operation/ilwisoperation.h
@@ -1,6 +1,10 @@
 #ifndef ILWISOPERATION_H
 #define ILWISOPERATION_H
 
+#include <functional>
+#include <future>
+#include <memory>
+
 #include "OperationExpression.h"
 #include "operationmetadata.h"
 #include "operation.h"
