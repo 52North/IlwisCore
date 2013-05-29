a1a9dca3b826707fbe234b0b5bbbaea10d955a37
MSchouwenburg <m.schouwenburg@utwente.nl>
2013-05-29 09:48:13 +0200

    created

---
+++ core/util/geometries.h
@@ -0,0 +1,23 @@
+#ifndef GEOMETRIES_H
+#define GEOMETRIES_H
+
+#include <type_traits>
+
+#include "angle.h"
+#include "point.h"
+
+//#include <boost/geometry.hpp>
+#include <boost/geometry/io/wkt/wkt.hpp>
+#include <boost/geometry/geometries/box.hpp>
+#include <boost/geometry/geometries/register/box.hpp>
+
+#include <boost/geometry/geometries/linestring.hpp>
+#include <boost/geometry/geometries/register/linestring.hpp>
+#include <boost/geometry/geometries/polygon.hpp>
+
+
+#include "box.h"
+#include "line.h"
+#include "polygon.h"
+
+#endif // GEOMETRIES_H
---
+++ core/util/polygon.h
@@ -0,0 +1,8 @@
+#ifndef POLYGON_H
+#define POLYGON_H
+
+namespace Ilwis {
+typedef boost::geometry::model::polygon<Coordinate> Polygon;
+}
+
+#endif // POLYGON_H
