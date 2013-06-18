90997baab3abecb2c3eff5bc9b3fe6d1b5e7e1a2
MSchouwenburg <m.schouwenburg@utwente.nl>
2013-06-18 14:39:41 +0200

    added the adapter and uses pixel_d in its interfaces

--- core/ilwisobjects/geometry/georeference/georeference.cpp
+++ core/ilwisobjects/geometry/georeference/georeference.cpp
@@ -54,4 +54,36 @@ void GeoReference::centerOfPixel(bool yesno)
     _centerOfPixel = true;
 }
 
+void GeoReference::adapter(GeoRefAdapter *adapt)
+{
+    _adapter.reset(adapt);
+}
+
+const QScopedPointer<Ilwis::GeoRefAdapter> &GeoReference::adapter() const
+{
+    return _adapter;
+}
+
+Box2D<double> GeoReference::pixel2Coord(const Box2D<qint32> &box) const
+{
+    if ( !box.isValid()) {
+        ERROR2(ERR_INVALID_PROPERTY_FOR_2,"size", "box");
+        return Box2D<double>();
+    }
+    Coordinate c1 = pixel2Coord(box.min_corner());
+    Coordinate c2 = pixel2Coord(box.max_corner());
+    return Box2D<double>(c1,c2);
+}
+
+Box2D<qint32> GeoReference::coord2Pixel(const Box2D<double> &box) const
+{
+    if ( !box.isValid()) {
+        ERROR2(ERR_INVALID_PROPERTY_FOR_2,"size", "box");
+        return  Box2D<qint32>();
+    }
+    Pixel p1 = coord2Pixel(box.min_corner());
+    Pixel p2 = coord2Pixel(box.max_corner());
+    return Box2D<qint32>(p1,p2);
+}
+
 
--- core/ilwisobjects/geometry/georeference/georeference.h
+++ core/ilwisobjects/geometry/georeference/georeference.h
@@ -2,18 +2,20 @@
 #define GEOREFERENCE_H
 
 #include "Kernel_global.h"
+#include "georefadapter.h"
 
 namespace Ilwis {
+
 class KERNELSHARED_EXPORT GeoReference : public IlwisObject
 {
 public:
     GeoReference();
     GeoReference(const Resource& res);
 
-    virtual Coordinate pixel2Coord(const Pixel&) const = 0;
-    virtual Point2D<double> coord2Pixel(const Coordinate& crd) const = 0;
-    virtual Box2D<double> pixel2Coord(const Box2D<qint32>& box ) const = 0;
-    virtual Box2D<qint32> coord2Pixel(const Box2D<double>& box) const = 0;
+    virtual Coordinate pixel2Coord(const Pixel_d&) const = 0;
+    virtual Pixel_d coord2Pixel(const Coordinate& crd) const = 0;
+    virtual Box2D<double> pixel2Coord(const Box2D<qint32>& box ) const;
+    virtual Box2D<qint32> coord2Pixel(const Box2D<double>& box) const;
     ICoordinateSystem coordinateSystem() const;
     void coordinateSystem(const ICoordinateSystem& csy);
     virtual double pixelSize() const = 0;
@@ -22,11 +24,14 @@ public:
     void size(const Size& sz);
     bool centerOfPixel() const;
     void centerOfPixel(bool yesno);
-
+    void adapter(GeoRefAdapter* adapt);
+protected:
+    const QScopedPointer<GeoRefAdapter> &adapter() const;
 private:
     ICoordinateSystem _csy;
     Size _size;
     bool _centerOfPixel;
+    QScopedPointer<GeoRefAdapter> _adapter;
 };
 
 typedef IlwisData<GeoReference> IGeoReference;
