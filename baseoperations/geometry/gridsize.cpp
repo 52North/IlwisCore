16e903873f796d5eaf361809b930b71abcf34a2f
Martin Schouwenburg <mschouwen@gmail.com>
2013-06-28 23:27:49 +0200

    created

---
+++ baseoperations/geometry/gridsize.cpp
@@ -0,0 +1,86 @@
+#include <functional>
+#include <future>
+#include "kernel.h"
+#include "raster.h"
+#include "symboltable.h"
+#include "ilwisoperation.h"
+#include "gridsize.h"
+
+using namespace Ilwis;
+using namespace BaseOperations;
+
+GridSize::GridSize()
+{
+}
+
+GridSize::GridSize(quint64 metaid, const Ilwis::OperationExpression &expr) : OperationImplementation(metaid, expr)
+{
+}
+
+bool GridSize::execute(ExecutionContext *ctx, SymbolTable &symTable)
+{
+    if (_prepState == sNOTPREPARED)
+        if((_prepState = prepare(ctx, symTable)) != sPREPARED)
+            return false;
+
+    Size sz = _inputGC->size();
+    int dimsize = 0;
+    if ( _dim == "xsize")
+        dimsize =  sz.xsize();
+    else if ( _dim == "ysize")
+        dimsize =  sz.ysize();
+    else if ( _dim == "zsize")
+        dimsize = sz.zsize();
+    QVariant var = dimsize;
+    ctx->addOutput(symTable,var,sUNDEF,itINT32, Resource());
+
+    return true;
+}
+
+OperationImplementation *GridSize::create(quint64 metaid, const Ilwis::OperationExpression &expr)
+{
+    return new GridSize(metaid, expr);
+}
+
+OperationImplementation::State GridSize::prepare(ExecutionContext *, const SymbolTable& ){
+    QString gc = _expression.parm(0).value();
+    if (!_inputGC.prepare(gc)) {
+        ERROR2(ERR_COULD_NOT_LOAD_2,gc,"");
+        return sPREPAREFAILED;
+    }
+     QString dim  =_expression.parm(1).value().toLower();
+     if ( dim != "xsize" && dim != "ysize" && dim != "zsize") {
+         ERROR2(ERR_ILLEGAL_VALUE_2,"dimension value",dim);
+         return sPREPAREFAILED;
+     }
+     _dim = dim;
+
+     return sPREPARED;
+
+}
+
+quint64 GridSize::createMetadata()
+{
+    QString url = QString("ilwis://operations/gridsize");
+    Resource res(QUrl(url), itOPERATIONMETADATA);
+    res.addProperty("namespace","ilwis");
+    res.addProperty("longname","gridsize");
+    res.addProperty("syntax","gridsize(gridcoverage,xsize|ysize|zsize)");
+    res.addProperty("inparameters","2");
+    res.addProperty("pin_1_type", itGRIDCOVERAGE);
+    res.addProperty("pin_1_name", TR("gridcoverage"));
+    res.addProperty("pin_1_desc",TR("gridcoverage from which a dimension size is retrieved"));
+    res.addProperty("pin_2_type", itSTRING);
+    res.addProperty("pin_2_name", TR("dimension"));
+    res.addProperty("pin_2_desc",TR("Dimension"));
+    res.addProperty("outparameters",1);
+    res.addProperty("pout_1_type", itINT32);
+    res.addProperty("pout_1_name", TR("size"));
+    res.addProperty("pout_1_desc",TR("Lenght in one dimension meausred in grid cells"));
+    res.prepare();
+    url += "=" + QString::number(res.id());
+    res.setUrl(url);
+
+    mastercatalog()->addItems({res});
+    return res.id();
+}
---
+++ baseoperations/geometry/gridsize.h
@@ -0,0 +1,24 @@
+#ifndef GRIDSIZE_H
+#define GRIDSIZE_H
+
+namespace Ilwis {
+namespace BaseOperations {
+class GridSize : public OperationImplementation
+{
+public:
+    GridSize();
+    GridSize(quint64 metaid, const Ilwis::OperationExpression &expr);
+
+    bool execute(ExecutionContext *ctx, SymbolTable& symTable);
+    static OperationImplementation * create(quint64 metaid,const Ilwis::OperationExpression& expr);
+    static quint64 createMetadata();
+    OperationImplementation::State prepare(ExecutionContext *, const SymbolTable &);
+private:
+    IGridCoverage _inputGC;
+    QString _dim;
+
+};
+}
+}
+
+#endif // GRIDSIZE_H
