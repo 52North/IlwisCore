cc93921380a3ed5ca1123f776f8748ae3f8ec53a
Martin Schouwenburg <mschouwen@gmail.com>
2013-06-30 22:47:24 +0200

    added operations

--- baseoperations/baseoperationsmodule.cpp
+++ baseoperations/baseoperationsmodule.cpp
@@ -11,6 +11,7 @@
 #include "math/binarylogical.h"
 #include "geometry/resampleraster.h"
 #include "data/rasvalue.h"
+#include "data/ifoperation.h"
 #include "data/iffraster.h"
 #include "data/assignment.h"
 #include "data/selection.h"
@@ -20,6 +21,7 @@
 #include "geometry/coord2pixel.h"
 #include "geometry/gridsize.h"
 #include "util/text2output.h"
+#include "util/stringoperations.h"
 
 using namespace Ilwis;
 using namespace BaseOperations;
@@ -36,32 +38,36 @@ QString RasterOperationsModule::getInterfaceVersion() const
 
 void RasterOperationsModule::prepare()
 {
-    commandhandler()->addOperation(Sine::createMetadata(), Sine::create);
-    commandhandler()->addOperation(Cosine::createMetadata(), Cosine::create);
-    commandhandler()->addOperation(Tangent::createMetadata(), Tangent::create);
-    commandhandler()->addOperation(Arcsine::createMetadata(), Arcsine::create);
     commandhandler()->addOperation(Arccosine::createMetadata(), Arccosine::create);
+    commandhandler()->addOperation(Arcsine::createMetadata(), Arcsine::create);
     commandhandler()->addOperation(ArcTangent::createMetadata(), ArcTangent::create);
-    commandhandler()->addOperation(Log10::createMetadata(), Log10::create);
-    commandhandler()->addOperation(NaturalLogarithm::createMetadata(), NaturalLogarithm::create);
-    commandhandler()->addOperation(Ceil::createMetadata(), Ceil::create);
-    commandhandler()->addOperation(Floor::createMetadata(), Floor::create);
-    commandhandler()->addOperation(Sign::createMetadata(), Sign::create);
-    commandhandler()->addOperation(CosineH::createMetadata(), CosineH::create);
-    commandhandler()->addOperation(SineH::createMetadata(), SineH::create);
-    commandhandler()->addOperation(BinaryMath::createMetadata(), BinaryMath::create);
-    commandhandler()->addOperation(BinaryLogical::createMetadata(), BinaryLogical::create);
-    commandhandler()->addOperation(ResampleRaster::createMetadata(), ResampleRaster::create);
-    commandhandler()->addOperation(RasValue::createMetadata(), RasValue::create);
-    commandhandler()->addOperation(IffRaster::createMetadata(), IffRaster::create);
     commandhandler()->addOperation(Assignment::createMetadata(), Assignment::create);
-    commandhandler()->addOperation(Selection::createMetadata(), Selection::create);
+    commandhandler()->addOperation(BinaryLogical::createMetadata(), BinaryLogical::create);
+    commandhandler()->addOperation(BinaryMath::createMetadata(), BinaryMath::create);
+    commandhandler()->addOperation(Ceil::createMetadata(), Ceil::create);
+    commandhandler()->addOperation(Coord2Pixel::createMetadata(), Coord2Pixel::create);
+    commandhandler()->addOperation(Cosine::createMetadata(), Cosine::create);
+    commandhandler()->addOperation(CosineH::createMetadata(), CosineH::create);
+    commandhandler()->addOperation(Floor::createMetadata(), Floor::create);
     commandhandler()->addOperation(FCoordinate::createMetadata(), FCoordinate::create);
     commandhandler()->addOperation(FPixel::createMetadata(), FPixel::create);
-    commandhandler()->addOperation(Pixel2Coord::createMetadata(), Pixel2Coord::create);
-    commandhandler()->addOperation(Coord2Pixel::createMetadata(), Coord2Pixel::create);
-    commandhandler()->addOperation(Text2Output::createMetadata(), Text2Output::create);
     commandhandler()->addOperation(GridSize::createMetadata(), GridSize::create);
+    commandhandler()->addOperation(IffRaster::createMetadata(), IffRaster::create);
+    commandhandler()->addOperation(Log10::createMetadata(), Log10::create);
+    commandhandler()->addOperation(NaturalLogarithm::createMetadata(), NaturalLogarithm::create);
+    commandhandler()->addOperation(Pixel2Coord::createMetadata(), Pixel2Coord::create);
+    commandhandler()->addOperation(RasValue::createMetadata(), RasValue::create);
+    commandhandler()->addOperation(ResampleRaster::createMetadata(), ResampleRaster::create);
+    commandhandler()->addOperation(Selection::createMetadata(), Selection::create);
+    commandhandler()->addOperation(Sign::createMetadata(), Sign::create);
+    commandhandler()->addOperation(Sine::createMetadata(), Sine::create);
+    commandhandler()->addOperation(SineH::createMetadata(), SineH::create);
+    commandhandler()->addOperation(StringFind::createMetadata(), StringFind::create);
+    commandhandler()->addOperation(StringSub::createMetadata(), StringSub::create);
+    commandhandler()->addOperation(StringReplace::createMetadata(), StringReplace::create);
+    commandhandler()->addOperation(Tangent::createMetadata(), Tangent::create);
+    commandhandler()->addOperation(Text2Output::createMetadata(), Text2Output::create);
+
 }
 
 QString RasterOperationsModule::name() const
