e69b82b96861b7898ccfbdcc6bbe69d4e3196047
MSchouwenburg <m.schouwenburg@utwente.nl>
2013-07-02 15:39:23 +0200

    implemented the subdivide method and execute

--- core/ilwisobjects/operation/operationhelperfeatures.cpp
+++ core/ilwisobjects/operation/operationhelperfeatures.cpp
@@ -36,3 +36,23 @@ IIlwisObject OperationHelperFeatures::initialize(const IIlwisObject &inputObject
     }
     return obj;
 }
+
+int OperationHelperFeatures::subdivideTasks(ExecutionContext *ctx, const IFeatureCoverage &fcov, std::vector<std::vector<quint32>> &subsets)
+{
+    int nofFeatures = fcov->featureCount();
+    int cores = std::min(QThread::idealThreadCount(),nofFeatures);
+    if (nofFeatures < 1000 || ctx->_threaded == false)
+        cores = 1;
+
+    int block = 0;
+    subsets.resize(cores);
+    int blockBoundary = nofFeatures / cores;
+    for(int i=0; i<nofFeatures ; ++i) {
+        subsets[block].push_back(i);
+        if ( (i!= 0 && i % blockBoundary == 0) && block != cores - 1) { // last block maybe indefinitly big, catches rounding errors
+            ++block;
+        }
+    }
+
+    return cores;
+}
--- core/ilwisobjects/operation/operationhelperfeatures.h
+++ core/ilwisobjects/operation/operationhelperfeatures.h
@@ -10,8 +10,25 @@ class KERNELSHARED_EXPORT OperationHelperFeatures
 public:
     OperationHelperFeatures();
     static IIlwisObject initialize(const IIlwisObject &inputObject, IlwisTypes tp, quint64 what);
-    template<typename T> static bool execute(ExecutionContext* ctx, T func, IFeatureCoverage& outputGC, std::vector<quint32> subset=std::vector<quint32>()){
-        return false;
+    static int subdivideTasks(ExecutionContext *ctx, const IFeatureCoverage& gcov, std::vector<std::vector<quint32> > &subsets);
+    template<typename T> static bool execute(ExecutionContext* ctx, T func, IFeatureCoverage& inputFC){
+        std::vector<std::vector<quint32>> subsets;
+
+        int cores = OperationHelperFeatures::subdivideTasks(ctx,inputFC, subsets);
+        if ( cores == iUNDEF)
+            return false;
+
+        std::vector<std::future<bool>> futures(cores);
+        bool res = true;
+
+        for(int i =0; i < cores; ++i) {
+            futures[i] = std::async(std::launch::async, func, subsets[i]);
+        }
+
+        for(int i =0; i < cores; ++i) {
+            res &= futures[i].get();
+        }
+        return true;
     }
 };
 }
