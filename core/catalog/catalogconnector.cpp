6c109c1e13960fed49e5a2e3e40c29d155b2cf69
Martin Schouwenburg <mschouwen@gmail.com>
2013-11-10 22:30:43 +0100

    integrated containerConnector

--- core/catalog/catalogconnector.cpp
+++ core/catalog/catalogconnector.cpp
@@ -7,17 +7,44 @@
 #include "identity.h"
 #include "factory.h"
 #include "abstractfactory.h"
-#include "catalog.h"
-#include "connectorfactory.h"
 #include "connectorinterface.h"
+#include "connectorfactory.h"
+#include "containerconnector.h"
+#include "catalog.h"
 #include  "resource.h"
 #include "catalogconnector.h"
 
 using namespace Ilwis;
 
+CatalogConnector::CatalogConnector(const Resource &resource) : _location(resource)
+{
+    const ConnectorFactory *factory = kernel()->factory<ConnectorFactory>("ConnectorFactory",resource);
+
+    if ( factory)
+        _containerconnector.reset(dynamic_cast<ContainerConnector *>(factory->createSuitable(Resource(resource.url(), itCONTAINER))));
+    else {
+        kernel()->issues()->log(TR("Cann't find suitable factory for %1 ").arg(resource.name()));
+    }
+}
+
 Resource &CatalogConnector::source()
 {
     return _location;
+}
+
+bool CatalogConnector::isValid() const
+{
+    return _location.isValid() && _containerconnector;
+}
+
+std::unique_ptr<ContainerConnector> &CatalogConnector::containerConnector()
+{
+    return _containerconnector;
+}
+
+const std::unique_ptr<ContainerConnector> &CatalogConnector::containerConnector() const
+{
+    return _containerconnector;
 }
 
 ConnectorInterface *CatalogConnector::clone() const
--- core/catalog/catalogconnector.h
+++ core/catalog/catalogconnector.h
@@ -2,19 +2,22 @@
 #define CATALOGCONNECTOR_H
 
 #include "Kernel_global.h"
+#include <QStringList>
 
 namespace Ilwis {
 
 class Catalog;
+class ContainerConnector;
 
 
 class KERNELSHARED_EXPORT CatalogConnector : public ConnectorInterface
 {
 public:
-    CatalogConnector(const Resource& resource) : _location(resource) {}
+    CatalogConnector(const Resource& resource);
     virtual ~CatalogConnector() {}
 
     virtual bool loadItems() = 0;
+    virtual QStringList itemlist() const {return QStringList();}
     /*!
      \brief adds a filter to set of filters which this connector will use when adding items to a catalog
 
@@ -24,10 +27,17 @@ public:
     */
     ConnectorInterface* clone() const;
 
-    virtual bool canUse(const QUrl& resource) const = 0;
+    virtual bool canUse(const Resource& resource) const = 0;
     Resource& source();
+    bool isValid() const;
 protected:
      Resource _location;
+
+     std::unique_ptr<ContainerConnector>& containerConnector();
+     const std::unique_ptr<ContainerConnector>& containerConnector() const;
+
+private:
+    std::unique_ptr<ContainerConnector> _containerconnector;
 };
 
 }
