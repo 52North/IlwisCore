8306cc5870ff9d69a660f192775fdb5c3aa1f5ff
MSchouwenburg <m.schouwenburg@utwente.nl>
2013-11-12 15:46:37 +0100

    added pure isValid methid

--- core/catalog/containerconnector.h
+++ core/catalog/containerconnector.h
@@ -14,12 +14,9 @@ public:
     virtual bool prepare() = 0;
     virtual std::vector<QUrl> sources(const QStringList& filter, int options=foFULLPATHS) const = 0;
     virtual QFileInfo toLocalFile(const QUrl& datasource) const = 0;
-//    virtual bool readText(const QUrl& source, QString& result,quint32 len=-1) const { return false;}
-//    virtual quint64 readBinary(char * , qint64 ) { return false; }
-//    virtual bool connectTo(const QString &filename, OpenMode mode=omBINARY) = 0;
-//    virtual void disconnect() = 0;
     Resource& source() ;
     Resource source() const;
+    virtual bool isValid() const = 0;
 
 private:
     Resource _container;
