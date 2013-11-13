a7e7b021d649a3d099a7116e5dada12807844dbe
JeroenBrinkman <s1366491@student.utwente.nl>
2013-11-13 15:39:43 +0100

    more doxygen improvements

--- core/ilwisobjects/ilwisobject.h
+++ core/ilwisobjects/ilwisobject.h
@@ -31,7 +31,7 @@ typedef IlwisTypes (*IlwisTypeFunction)(const QString& resource);
  * one instance of it. The purpose of the class is to ensure automatic consistency of data in the system. Changes to one
  * reference to the object are automatically reflected in the others because they are the same object.
  *
- * A IlwisObject has a IlwisType, which can be any of the following, or a combination thereof:
+ * A IlwisObject has a IlwisTypes, which can be any of the following, or a combination thereof:
  *
  * - itUNKNOWN : type not known
  * - itPOINT : a point (on a map)
@@ -49,13 +49,13 @@ typedef IlwisTypes (*IlwisTypeFunction)(const QString& resource);
  * - itCONVENTIONALCOORDSYSTEM : Projected coordinate system or lat-lon
  * - itORTHOCOORDSYSTEM : -
  * - itELLIPSOID : a geographical ellipsoid
- * - itPROJECTION :
+ * - itPROJECTION : projection of coordinates
  * - itDATABASETABLE : a table from a database
  * - itFLATTABLE : a table from memory
- * - itREPRESENTATION :
- * - itGEOREF :
+ * - itREPRESENTATION : -
+ * - itGEOREF : pixel to coordinate and the other way around
  * - itENVELOPE : Bounding Box
- * - itOPERATIONMETADATA :
+ * - itOPERATIONMETADATA : operation metadata
  * - itRASTERSIZE : the size in pixels of a raster
  * - itCATALOG : a collection of ilwisobjects
  * - itCONTAINER : external organisation of the ilwisobjects
@@ -88,7 +88,7 @@ typedef IlwisTypes (*IlwisTypeFunction)(const QString& resource);
  * - itTIMEITEM : an item on a time domain
  * - itCOLOR : an item on a color domain
  * - itANY : the wildcard type
-*/
+ */
 class KERNELSHARED_EXPORT IlwisObject : public QObject, public Identity
 {
     Q_OBJECT
@@ -103,135 +103,265 @@ public:
      *connector maybe for input/output or both. This enum describes how a connector can be used.
      */
     enum ConnectorMode{cmINPUT=1, cmOUTPUT=2, cmEXTENDED=4};
+
+    /**
+     * this enum describes how certain data should be stored, as metadata or as binary data
+     */
     enum StoreMode{smMETADATA=1, smBINARYDATA=2};
 
+    /**
+     * Standard constructor for an ilwisobject
+     */
     IlwisObject();
+
+    /**
+     * Constructor for an ilwis object using a Resource
+     * the resource will be used to fill the identity field of this object
+     *
+     *\sa Resource
+     * @param source the resource to be used
+     */
     IlwisObject(const Resource &source);
+
     virtual ~IlwisObject();
 
+    /**
+     * tries to Prepares this ilwisobject for use
+     * @return true when the prepare succeeded
+     */
     virtual bool prepare();
+
+    /**
+     * Changes the name of this object, new name cannot be null.
+     * neither can you change the name if the IlwisObject is readonly
+     *
+     * @param nm the new name
+     */
     void setName(const QString& nm);
+
+    /**
+     * Changes the code of this IlwisObject, can only happen when this object is not readonly,
+     * and the identity is not null
+     *
+     * @param cd the new code
+     */
     void setCode(const QString &cd);
 
-   /*!
-    \brief returns the moment when the object was last modified
+    /*!
+     *\brief returns the moment when the object was last modified
+     *
+     *\return QDateTime
+     */
+    QDateTime modifiedTime() const;
 
-    \return QDateTime
-   */
-   QDateTime modifiedTime() const;
 
-   /*!
-    \brief sets the last modified time of the object
+    /*!
+     *\brief sets the last modified time of the object
+     *
+     *\param time
+     */
+    void setModifiedTime(const Time& time);
 
-    \param time
-   */
-   void setModifiedTime(const Time& time);
-   Time createTime() const;
-   void setCreateTime(const Time& time);
+    /**
+     * returns the create time of this object
+     *
+     * @return Time
+     */
+    Time createTime() const;
 
-   /*!
-    \brief translates the defintion of an object to a string representation
+    /**
+     * Changes the create time of this object
+     *
+     * @param time
+     */
+    void setCreateTime(const Time& time);
 
-    \return QString
-   */
+    /*!
+     * \brief translates the defintion of an object to a string representation
+     *
+     *\return QString
+     */
+    virtual QString toString();
 
-   virtual QString toString();
+    /*!
+     * \brief connects an ilwisobject to an input or output source
+     *
+     * IlwisObjects are agnostic to where their data comes from. The connector isolates them from that. An object might
+     * have two connectors : one for input, one for output. Mostely though there is only one that can be used for both
+     * input and output ( if the connector allows it).
+     *
+     *\param connector the connector to be used
+     *\param input defines the nature of the connector.
+     */
+    virtual void setConnector(ConnectorInterface *connector, int mode=cmINPUT | cmOUTPUT);
 
-   /*!
-    \brief connects an ilwisobject to an input or output source
+    /*!
+     * \brief isEqual compares the properties of ilwisobjects to test for equality.
+     *
+     *It also will test the id's in which case the equality is guarenteed. The method is virtual so it will be re-implemented at each derivative.
+     * \param obj an ilwisobject to be compared with
+     * \return true when the objects are equal
+     */
+    virtual bool isEqual(const IlwisObject &obj) const;
 
-    IlwisObjects are agnostic to where their data comes from. The connector isolates them from that. An object might
-    have two connectors : one for input, one for output. Mostely though there is only one that can be used for both
-    input and output ( if the connector allows it).
+    /*!
+     * \brief isValid Tests the validity of an ilwisobject.
+     *
+     *Ilwisobjects are valid when all their properties are valid. Usualy this is determined during the 'loadXXX' methods that fill the properties of an object
+     * \return valid or not
+     */
+    virtual bool isValid() const;
 
-    \param connector the connector to be used
-    \param input defines the nature of the connector.
-   */
-   virtual void setConnector(ConnectorInterface *connector, int mode=cmINPUT | cmOUTPUT);
+    /*!
+     * \brief isReadOnly tells if the source/target of the ilwisobject maybe modifiable.
+     *
+     * This method returns the combination of the connectors readonly state and the objects readonly state. The connectors readonly state points usualy to a physical or other lower level readonly state. while the objects readonly state points to a logical state.
+     * \return readonly or not
+     */
+    virtual bool isReadOnly() const;
 
-   /*!
-    * \brief isEqual compares the properties of ilwisobjects to test for equality.
-    *
-    *It also will test the id's in which case the equality is guarenteed. The method is virtual so it will be re-implemented at each derivative.
-    * \param obj an ilwisobject to be compared with
-    * \return true when the objects are equal
-    */
-   virtual bool isEqual(const IlwisObject &obj) const;
-   /*!
-    * \brief isValid Tests the validity of an ilwisobject.
-    *
-    *Ilwisobjects are valid when all their properties are valid. Usualy this is determined during the 'loadXXX' methods that fill the properties of an object
-    * \return valid or not
-    */
-   virtual bool isValid() const;
-   /*!
-    * \brief isReadOnly tells if the source/target of the ilwisobject maybe modifiable.
-    *
-    * This method returns the combination of the connectors readonly state and the objects readonly state. The connectors readonly state points usualy to a physical or other lower level readonly state. while the objects readonly state points to a logical state.
-    * \return readonly or not
-    */
-   virtual bool isReadOnly() const;
-   virtual void setReadOnly(bool yesno);
-   virtual bool hasChanged() const;
-   virtual void changed(bool yesno) ;
+    /**
+     * Changes the readonly state of this object. in order to do this it will both change the connectors readonly state and the objects
+     *
+     * @param yesno the new readonly state
+     */
+    virtual void setReadOnly(bool yesno);
 
-   /**
-    * Gives the IlwisType of this object
-    * \sa IlwisObject
-    * @return the ilwisType of this IlwisObject
-    */
-   virtual IlwisTypes ilwisType() const = 0;
-   virtual bool prepare(const QString& def);
+    /**
+     * Checks if this object has been changed, requires the changed flag to be set during the actual change, so might not always be fully acurate
+     * @return true when the object has changed
+     */
+    virtual bool hasChanged() const;
 
-   bool fromInternal(const QSqlRecord& rec);
-   bool isAnonymous() const;
+    /**
+     * Sets the changed flag, needs to be set when something within this object gets changed
+     *
+     * @param yesno the new value of the changed flag
+     */
+    virtual void changed(bool yesno) ;
 
-   /*!
-    * \brief source the location of the source that represents the physical read origin of this object
-    * \return url that is sufficient to find the source
-    */
-   virtual Resource source(int mode=cmINPUT) const;
-   /*!
-    * \brief target the location of the source that represents the physical write target of this object
-    * \return url that is sufficient to find the target
-    */
-   virtual bool store(int mode=smMETADATA | smBINARYDATA);
-   /*!
-    connectTo allocates a connector of a certain format to the ilwisobject.
-    * \param url the source/target were the connector connects to. The url maybe empty, internally an url will be created based on the format, name and working catalog
-    * \param format the format identifying the source/target format. The name of the format is registered (plus namespace) by the connector plugin that realizes this format
-    * \param fnamespace an additional identifier for selecting the correct connector. format names maybe identical from different connectors. The combination namespace-format is unique accross the system
-    * \param cmode the mode of the connector to be created
-    */
-   void connectTo(const QUrl &url, const QString &format, const QString &fnamespace, ConnectorMode cmode);
+    /**
+     * Gives the IlwisTypes of this object
+     * \sa IlwisObject
+     * @return the IlwisTypes of this IlwisObject
+     */
+    virtual IlwisTypes ilwisType() const = 0;
 
-   /*!
-    \brief Factory method for creating IlwisObjects
+    /**
+     * ??
+     * @brief prepare
+     * @param def
+     * @return
+     */
+    virtual bool prepare(const QString& def);
 
-    \param id of a resource a Url describing a unique Ilwis object.
-    \param connectorType the connector that should handle this resource. If none is given ("default"), the system will figure it out by it self
-    \return IlwisObject a created ilwisobject of the specified type
-   */
-   virtual IlwisObject *clone() ;
-   virtual bool merge(const IlwisObject* obj, int options=0);
+    /**
+     * fills the name description code and connector field from a database record
+     * also sets the changed flag.
+     * will not succeed if this object is readonly
+     * @param rec the databaserecord (QSqlRecord)
+     * @return true when succeeded
+     */
+    bool fromInternal(const QSqlRecord& rec);
 
-   template<typename T> static T create(const QString& def){
-       T object;
-       object.prepare(def);
-       return object;
-   }
+    /**
+     * checks the name of this object for the ANONYMOUS_PREFIX, this prefix must be at position 0 to generate a positive result
+     *
+     * @return true when this object is ANONYMOUS
+     */
+    bool isAnonymous() const;
 
-   template<typename T> static T create(quint64 id){
-       T object;
-       object.prepare(id);
-       return object;
-   }
+    /*!
+     * \brief source the location of the source that represents the physical read origin of this object
+     * \return url that is sufficient to find the source
+     */
+    virtual Resource source(int mode=cmINPUT) const;
 
-   static IlwisObject *create(const Resource& source);
-   static IlwisTypes findType(const QString &source);
-   static QString type2Name(IlwisTypes t);
-   static IlwisTypes name2Type(const QString &name);
-   static void addTypeFunction(IlwisTypeFunction);
+    /*!
+     * \brief target the location of the source that represents the physical write target of this object
+     * \return url that is sufficient to find the target
+     */
+    virtual bool store(int mode=smMETADATA | smBINARYDATA);
+
+    /*!
+     connectTo allocates a connector of a certain format to the ilwisobject.
+     * \param url the source/target were the connector connects to. The url maybe empty, internally an url will be created based on the format, name and working catalog
+     * \param format the format identifying the source/target format. The name of the format is registered (plus namespace) by the connector plugin that realizes this format
+     * \param fnamespace an additional identifier for selecting the correct connector. format names maybe identical from different connectors. The combination namespace-format is unique accross the system
+     * \param cmode the mode of the connector to be created
+     */
+    void connectTo(const QUrl &url, const QString &format, const QString &fnamespace, ConnectorMode cmode);
+
+    /*!
+     *\brief Factory method for creating IlwisObjects
+     *
+     *\param id of a resource a Url describing a unique Ilwis object.
+     *\param connectorType the connector that should handle this resource. If none is given ("default"), the system will figure it out by it self
+     *\return IlwisObject a created ilwisobject of the specified type
+     */
+    virtual IlwisObject *clone() ;
+
+    /**
+     * Tries to merge 2 IlwisObjects together, template function does not work for standard IlwisObject, see the correspondending subclass for details
+     *
+     * @param obj the object this should merge with
+     * @param options merging options
+     * @return true when succesful
+     */
+    virtual bool merge(const IlwisObject* obj, int options=0);
+
+    template<typename T> static T create(const QString& def){
+        T object;
+        object.prepare(def);
+        return object;
+    }
+
+    template<typename T> static T create(quint64 id){
+        T object;
+        object.prepare(id);
+        return object;
+    }
+
+    /**
+     * Creates a new IlwisObject from the given Resource
+     * will fail if the resource is somehow invalid
+     *
+     * @param source the Resource
+     * @return a new ilwisObject when succesful, or 0 when it fails
+     */
+    static IlwisObject *create(const Resource& source);
+
+    /**
+      ??
+     * @brief findType
+     * @param source
+     * @return
+     */
+    static IlwisTypes findType(const QString &source);
+
+    /**
+     * creates a String with the name of the given IlwisTypes
+     * will return undefined if the given IlwisTypes is invalid
+     * \sa IlwisObject
+     * @param the IlwisTypes in need of naming
+     * @return A string with the name of the IlwisTypes
+     */
+    static QString type2Name(IlwisTypes t);
+
+    /**
+     * converts a string with a name into a IlwisTypes
+     * if the name is invalid it will return itUNKOWN
+     *
+     * @param name a string with the name of an IlwisTypes
+     * @return the correspondending IlwisTypes
+     */
+    static IlwisTypes name2Type(const QString &name);
+
+    /**
+      ??
+     * @brief addTypeFunction
+     */
+    static void addTypeFunction(IlwisTypeFunction);
 
 protected:
    QScopedPointer<ConnectorInterface>& connector(int mode=cmINPUT | cmOUTPUT);
