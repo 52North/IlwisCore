b0502d3dfd2adf6545a9b40a172f76299a69a65a
MSchouwenburg <m.schouwenburg@utwente.nl>
2013-11-18 15:53:16 +0100

    after scanning folder the resource is now correctly fetched from the mastercatalog with all the correct properties

--- core/ilwisobjects/ilwisdata.h
+++ core/ilwisobjects/ilwisdata.h
@@ -191,9 +191,11 @@ public:
 
     }
 
-    bool prepare(const Resource& resource){
-        mastercatalog()->addContainer(resource.container());
-        if (resource.isValid()) {
+    bool prepare(const Resource& resource1){
+        if (resource1.isValid()) {
+            mastercatalog()->addContainer(resource1.container());
+            quint64 id = mastercatalog()->url2id(resource1.url(), resource1.ilwisType());
+            Resource resource = mastercatalog()->id2Resource(id);
             if (!mastercatalog()->isRegistered(resource.id())) {
                 T *data = static_cast<T *>(IlwisObject::create(resource));
                 if ( data == 0) {
@@ -208,7 +210,7 @@ public:
             }
             return true;
         } else {
-            ERROR1(ERR_COULDNT_CREATE_OBJECT_FOR_1,resource.name());
+            ERROR1(ERR_COULDNT_CREATE_OBJECT_FOR_1,resource1.name());
         }
         return false;
 
