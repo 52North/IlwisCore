715bb3d4b0c1155808178acdffd7749b0888e6cb
pokulo@gmx.de <h.g.kiessling@utwente.nl>
2014-02-04 10:45:39 +0100

    merge in GeometryHelper::geometryType

--- core/ilwisobjects/coverage/geometryhelper.cpp
+++ core/ilwisobjects/coverage/geometryhelper.cpp
@@ -46,13 +46,6 @@ IlwisTypes GeometryHelper::geometryType(const geos::geom::Geometry *geom){
         return itLINE;
     case geos::geom::GEOS_POLYGON:
     case geos::geom::GEOS_MULTIPOLYGON:
-        return itPOLYGON;
-    case geos::geom::GEOS_MULTIPOINT:
-        return itPOINT;
-    case geos::geom::GEOS_MULTILINESTRING:
-        return itLINE;
-    case geos::geom::GEOS_MULTIPOLYGON:
-        return itPOLYGON;
     case geos::geom::GEOS_LINEARRING:
         return itPOLYGON;
     case geos::geom::GEOS_GEOMETRYCOLLECTION:
