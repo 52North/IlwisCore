cbaab606b82b94f2ca754ad4cbcf9e0b7a458f21
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2014-04-16 14:26:28 +0200

    new

---
+++ rasteroperations/classification/rasterclassification.h
@@ -0,0 +1,42 @@
+#ifndef RASTERCLASSIFICATION_H
+#define RASTERCLASSIFICATION_H
+
+namespace Ilwis {
+namespace RasterOperations {
+
+class Classifier;
+
+class RasterClassification : public OperationImplementation
+{
+public:
+    RasterClassification();
+    RasterClassification(quint64 metaid, const Ilwis::OperationExpression &expr);
+    bool execute(ExecutionContext *ctx,SymbolTable& symTable);
+
+protected:
+    SampleSet _sampleSet;
+    IRasterCoverage _outputRaster;
+    std::unique_ptr<Classifier> _classifier;
+    Ilwis::OperationImplementation::State prepare(ExecutionContext *, const SymbolTable &sym);
+};
+
+class BoxClassification : public RasterClassification {
+public:
+    BoxClassification();
+    BoxClassification(quint64 metaid, const Ilwis::OperationExpression &expr);
+
+    //bool execute(ExecutionContext *ctx,SymbolTable& symTable);
+    static Ilwis::OperationImplementation *create(quint64 metaid,const Ilwis::OperationExpression& expr);
+    Ilwis::OperationImplementation::State prepare(ExecutionContext *ctx, const SymbolTable &sym);
+
+    static quint64 createMetadata();
+
+    NEW_OPERATION(BoxClassification);
+
+private:
+    double _widenFactor;
+};
+}
+}
+
+#endif // RASTERCLASSIFICATION_H
