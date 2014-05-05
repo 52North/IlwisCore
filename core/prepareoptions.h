f9c3abe02f0304ff6dd4d42b986e00e50047332d
Martin Schouwenburg <mschouwen@gmail.com>
2014-05-05 16:01:33 +0200

    added some constants for implicitparameters

--- core/prepareoptions.h
+++ core/prepareoptions.h
@@ -16,6 +16,12 @@ struct PrepareOptions {
 
     std::map<QString, QVariant> _values;
 };
+
+const QString IMPLICITPARMATER="_implicitparm_";
+const QString IMPLICITPARMATER0="_implicitparm_0";
+const QString IMPLICITPARMATER1="_implicitparm_1";
+const QString IMPLICITPARMATER2="_implicitparm_2";
+const QString IMPLICITPARMATER3="_implicitparm_3";
 }
 
 #endif // PREPARATIONPARAMETERS_H
