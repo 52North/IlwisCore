be3d8c82be11886e1418a8d31b96ed08b1b25a2c
Martin Schouwenburg <mschouwen@gmail.com>
2014-05-05 15:55:32 +0200

    added handling of indexlist as return type

--- ilwisscript/ast/expressionnode.cpp
+++ ilwisscript/ast/expressionnode.cpp
@@ -1,8 +1,9 @@
-#include "ilwis.h"
+#include <set>
+#include "kernel.h"
 #include "astnode.h"
 #include "operationnode.h"
-#include "expressionnode.h"
 #include "symboltable.h"
+#include "expressionnode.h"
 
 using namespace Ilwis;
 
@@ -41,6 +42,8 @@ bool ExpressionNode::evaluate(SymbolTable &symbols, int scope, ExecutionContext 
     return ret;
 }
 
+
+
 bool ExpressionNode::handleAnd(int index, const NodeValue& vright,SymbolTable &symbols, ExecutionContext *ctx) {
     if ( vright.canConvert(index,QVariant::Bool) && _value.canConvert(index,QVariant::Bool)) {
         _value =  {_value.toBool(index) &&  vright.toBool(index), NodeValue::ctBOOLEAN};
@@ -48,7 +51,21 @@ bool ExpressionNode::handleAnd(int index, const NodeValue& vright,SymbolTable &s
     } else  if ( SymbolTable::isIntegerNumerical(vright) && SymbolTable::isIntegerNumerical(_value)){
         _value = {_value.toLongLong(index) & vright.toLongLong(index), NodeValue::ctNumerical};
         return true;
-    }
+    } else if ( SymbolTable::isIndex(index, vright) && SymbolTable::isIndex(index, _value)){
+        RelationFunc relation = [](const Indices& index1, const Indices& index2) {
+            std::set<quint32> resultSet;
+            for(auto i1 : index1){
+                for(auto i2 : index2){
+                    if ( i1 == i2)
+                        resultSet.insert(i1);
+                }
+            }
+            return resultSet;
+        };
+        handleIndexes(index, vright, relation);
+
+        return true;
+   }
    return handleBinaryCases(index, vright, "binarylogicalraster", "and", symbols, ctx);
 }
 
@@ -58,6 +75,18 @@ bool ExpressionNode::handleOr(int index,const NodeValue& vright,SymbolTable &sym
         return true;
     } else  if ( SymbolTable::isIntegerNumerical(vright) && SymbolTable::isIntegerNumerical(_value)){
         _value = {_value.toLongLong(index) | vright.toLongLong(index), NodeValue::ctNumerical};
+        return true;
+    } else if ( SymbolTable::isIndex(index, vright) && SymbolTable::isIndex(index, _value)){
+        RelationFunc relation = [](const Indices& index1, const Indices& index2) {
+            std::set<quint32> resultSet;
+            for(auto i1 : index1)
+                resultSet.insert(i1);
+            for(auto i2 : index2)
+                resultSet.insert(i2);
+            return resultSet;
+        };
+        handleIndexes(index, vright, relation);
+
         return true;
     }
     return handleBinaryCases(index, vright, "binarylogicalraster", "or", symbols, ctx);
@@ -70,6 +99,25 @@ bool ExpressionNode::handleXor(int index,const NodeValue& vright,SymbolTable &sy
     } else  if ( SymbolTable::isIntegerNumerical(vright) &&SymbolTable::isIntegerNumerical(_value)){
         _value = {_value.toLongLong(index) ^ vright.toLongLong(index), NodeValue::ctNumerical};
         return true;
+    } else if ( SymbolTable::isIndex(index, vright) && SymbolTable::isIndex(index, _value)){
+        RelationFunc relation = [](const Indices& index1, const Indices& index2) {
+            std::set<quint32> resultSet;
+            std::map<quint32,quint32> combos;
+            for(auto i1 : index1)
+                combos[i1]++;
+            for(auto i2 : index2)
+                combos[i2]++;
+            for(auto kvp : combos){
+                if ( kvp.second == 1)
+                    resultSet.insert(kvp.first);
+            }
+            return resultSet;
+        };
+        handleIndexes(index, vright, relation);
+
+        return true;
     }
     return handleBinaryCases(index,vright, "binarylogicalraster", "xor", symbols, ctx);
 }
+
+
