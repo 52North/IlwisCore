73001912e0c32e8d5067f4bc50e7b6f8f679cf13
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2014-05-15 11:41:11 +0200

    casing

--- baseoperations/geometry/contains.cpp
+++ baseoperations/geometry/contains.cpp
@@ -11,7 +11,7 @@
 #include "feature.h"
 #include "featurecoverage.h"
 #include "symboltable.h"
-#include "OperationExpression.h"
+#include "operationExpression.h"
 #include "operationmetadata.h"
 #include "operation.h"
 #include "operationhelper.h"
