4a71faa5712490fe5c3a598b864f8c005667f791
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2014-05-15 11:56:30 +0200

    OS dependent

--- ilwisscript/parserlexer/runtimechecks.cpp
+++ ilwisscript/parserlexer/runtimechecks.cpp
@@ -1,4 +1,5 @@
 extern "C" {
+#ifdef Q_OS_WIN
 #define NULL 0
 typedef int (__cdecl *_RTC_error_fn)(int, const char *, int, const char *, const char *, ...);
 _RTC_error_fn _CRT_RTC_INITW(void *res0, void **res1, int res2, int res3, int res4);
@@ -7,6 +8,7 @@ _RTC_error_fn _CRT_RTC_INITW(void *res0, void **res1, int res2, int res3, int re
 {
 return NULL;
 }
+#endif
 
 
 
