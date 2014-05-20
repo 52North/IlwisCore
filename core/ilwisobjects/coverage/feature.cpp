0fd51260d5d9df8e3ec8cf094398f7929e3d9f5d
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2014-05-20 10:09:35 +0200

    removed unneeded comments

--- core/ilwisobjects/coverage/feature.cpp
+++ core/ilwisobjects/coverage/feature.cpp
@@ -176,7 +176,7 @@ QVariant Feature::cell(quint32 colIndex, int index, bool asRaw)
     }
     if ( index >= 0 && index < _track.size())
         return _track[index]->cell(colIndex, index, asRaw);
-    return QVariant();//TODO: shouldn't this raise a std::out_of_range exception or similar?
+    return QVariant();
 }
 
 QVariant Feature::cell(const QString &name, int index, bool asRaw)
@@ -187,7 +187,7 @@ QVariant Feature::cell(const QString &name, int index, bool asRaw)
     }
     if ( index >= 0 && index < _track.size())
         return _track[index]->cell(name, index, asRaw);
-    return QVariant();//TODO: shouldn't this raise a std::out_of_range exception or similar?
+    return QVariant();
 }
 
 void Feature::setCell(const QString &name, const QVariant &var, int index)
