63c9919dde18f2f9e176c8148b7224dea87364dc
--global <--global>
2014-06-04 09:05:26 +0200

    a

--- core/ilwisobjects/table/flattable.cpp
+++ core/ilwisobjects/table/flattable.cpp
@@ -207,8 +207,6 @@ QVariant FlatTable::cell(const QString& col, quint32 rec, bool asRaw) const {
 
 QVariant FlatTable::cell(const quint32 index, quint32 rec, bool asRaw) const
 {
-    QVariant v = _datagrid[rec][index];
-    Time tm = v.value<Ilwis::Time>();
     if (!const_cast<FlatTable *>(this)->initLoad())
         return QVariant();
 
