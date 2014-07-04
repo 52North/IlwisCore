4cb2d440da44bd805b164d340c5758abaf3f1b16
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2014-07-04 14:24:32 +0200

    configuration class for ilwis based on the boost property trees

---
+++ core/util/ilwisconfiguration.h
@@ -0,0 +1,35 @@
+#ifndef ILWISCONFIGURATION_H
+#define ILWISCONFIGURATION_H
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+namespace Ilwis {
+class IlwisConfiguration
+{
+public:
+    IlwisConfiguration();
+    bool prepare(const QString& configlocation);
+
+    template<typename ValueType> ValueType operator ()(const QString& key, const ValueType& defaultValue){
+        QString tempkey = key;
+        tempkey.replace("/",".");
+        tempkey = "config." + tempkey;
+        ValueType v = _configuration.get(tempkey.toStdString(),defaultValue);
+        return v;
+    }
+
+private:
+   boost::property_tree::ptree _configuration;
+};
+
+template<> inline QString IlwisConfiguration::operator ()(const QString& key, const QString& defaultValue){
+    QString tempkey = key;
+    tempkey.replace("/",".");
+    tempkey = "config." + tempkey;
+    std::string v = _configuration.get(tempkey.toStdString(),defaultValue.toStdString());
+    return QString::fromStdString(v);
+}
+}
+
+#endif // ILWISCONFIGURATION_H
