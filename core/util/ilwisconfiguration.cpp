f172a8a917c0098d33d9f233f6865d1079ccd3ad
Henning Bredel <h.bredel@52north.org>
2014-07-15 15:47:29 +0200

    format code block

--- core/util/ilwisconfiguration.cpp
+++ core/util/ilwisconfiguration.cpp
@@ -11,15 +11,15 @@ IlwisConfiguration::IlwisConfiguration()
 bool IlwisConfiguration::prepare(const QString &configlocation)
 {
     try{
-    std::ifstream inputfile;
-    inputfile.open(configlocation.toStdString().c_str(),std::ifstream::in);
-    if (!inputfile.is_open()) {
-        return WARN2(ERR_COULD_NOT_LOAD_2,TR("configuration file"),configlocation);
-    }
+        std::ifstream inputfile;
+        inputfile.open(configlocation.toStdString().c_str(),std::ifstream::in);
+        if (!inputfile.is_open()) {
+            return WARN2(ERR_COULD_NOT_LOAD_2,TR("configuration file"),configlocation);
+        }
 
-    boost::property_tree::json_parser::read_json(inputfile,_configuration);
+        boost::property_tree::json_parser::read_json(inputfile,_configuration);
 
-    return true;
+        return true;
     } catch(const boost::property_tree::json_parser::json_parser_error& err){
         QString message = QString::fromStdString(err.message());
         return WARN2(ERR_NO_INITIALIZED_1,TR("configuration"),message);
