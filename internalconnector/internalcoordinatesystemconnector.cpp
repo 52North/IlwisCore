49e061988b646bf9ebd5ad3037d44e92b0dbca18
Martin Schouwenburg <mschouwen@gmail.com>
2014-07-16 20:06:09 +0200

    loadoptions added to interface of loaddata

--- internalconnector/internalcoordinatesystemconnector.cpp
+++ internalconnector/internalcoordinatesystemconnector.cpp
@@ -35,7 +35,7 @@ IlwisObject *InternalCoordinatesystemConnector::create() const
     return 0;
 }
 
-bool InternalCoordinatesystemConnector::loadData(IlwisObject *obj)
+bool InternalCoordinatesystemConnector::loadData(IlwisObject *obj, const LoadOptions& options )
 {
     return true;
 }
