3466bc68228ce72062bcc471d933aa8f9da5c0e3
Martin Schouwenburg <mschouwen@gmail.com>
2014-08-02 09:21:08 +0200

    the colorrange class is now renamed to colorrangebase and is no longer derived from Range. The derivation caused problems with childeren of this class which were also derived from range

--- core/ilwisobjects/domain/colorrange.h
+++ core/ilwisobjects/domain/colorrange.h
@@ -8,12 +8,13 @@ namespace Ilwis{
 class ColorItem;
 typedef QSharedPointer<ColorItem> SPColorItem;
 
-class KERNELSHARED_EXPORT ColorRange : public Range
+
+class KERNELSHARED_EXPORT ColorRangeBase
 {
 public:
     enum ColorModel{cmNONE, cmRGBA, cmHSLA, cmCYMKA, cmGREYSCALE};
 
-    ColorRange(IlwisTypes tp, ColorModel clrmodel);
+    ColorRangeBase(IlwisTypes tp, ColorModel clrmodel);
     ColorModel defaultColorModel() const;
     void defaultColorModel(ColorModel m);
 
@@ -29,33 +30,36 @@ private:
 
 };
 
-class KERNELSHARED_EXPORT ContinousColorRange  : public ColorRange{
+class KERNELSHARED_EXPORT ContinousColorRange  : public Range, public ColorRangeBase{
 public:
     ContinousColorRange();
-    ContinousColorRange(const QColor& clr1, const QColor& clr2=QColor("#FFFFFF"), ColorModel colormodel=ColorRange::cmRGBA);
+    ContinousColorRange(const QColor& clr1, const QColor& clr2=QColor("#FFFFFF"), ColorModel colormodel=ColorRangeBase::cmRGBA);
     bool isValid() const;
     QString toString() const;
     Range *clone() const;
     QVariant ensure(const QVariant& v, bool inclusive = true) const;
     bool contains(const QVariant& v, bool inclusive = true) const;
-    bool contains(ColorRange *v, bool inclusive = true) const;
+    bool contains(const Range *v, bool inclusive = true) const;
     QVariant impliedValue(const QVariant& v) const;
     IlwisTypes valueType() const;
-    QColor valueAt(quint32& index, const Ilwis::Range *rng);
+    static QColor valueAt(quint32& index, const Ilwis::Range *rng);
     quint32 count() const;
 private:
     QColor _limit1;
     QColor _limit2;
 };
 
-class ColorPalette : public ItemRange, public ColorRange  {
+class KERNELSHARED_EXPORT ColorPalette : public ItemRange, public ColorRangeBase  {
 public:
+    ColorPalette();
     QVariant impliedValue(const QVariant& v) const;
 
     SPDomainItem item(quint32 raw) const;
     SPDomainItem item(const QString &nam) const;
     SPDomainItem itemByOrder(quint32 index) const;
     QColor color(int index) const;
+    QString toString() const;
+    Range *clone() const;
 
     void add(DomainItem *item);
     void add(SPDomainItem item);
@@ -64,14 +68,17 @@ public:
 
     bool contains(const QVariant& color, bool inclusive = true) const;
     bool contains(SPRange rng, bool inclusive=true) const;
-    bool contains(ItemRange *rng, bool inclusive=true) const;
+    bool contains(Ilwis::Range *rng, bool inclusive=true) const;
 
     QVariant ensure(const QVariant& v, bool inclusive = true) const;
     quint32 count() const;
     IlwisTypes valueType() const;
+    static QColor valueAt(quint32& index, const Ilwis::Range *rng);
 
     bool isValid() const;
 
+    qint32 gotoIndex(qint32 index, qint32 step) const;
+    bool alignWithParent(const IDomain &dom);
 private:
     std::vector<SPColorItem> _colors;
 
