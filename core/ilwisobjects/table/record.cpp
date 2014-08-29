6e30a7101caf52af972c4faabbf0157ec99b8885
Henning Bredel <h.bredel@52north.org>
2014-08-29 17:09:26 +0200

    add readonly flag to columndefinition

--- core/ilwisobjects/table/record.cpp
+++ core/ilwisobjects/table/record.cpp
@@ -1,5 +1,11 @@
 #include "kernel.h"
+#include "ilwisdata.h"
+#include "domain.h"
+#include "range.h"
+#include "datadefinition.h"
+#include "columndefinition.h"
 #include "record.h"
+#include "table.h"
 
 using namespace Ilwis;
 
@@ -7,13 +13,14 @@ Record::Record()
 {
 }
 
-Record::Record(const std::vector<QVariant>& data)
+Record::Record(const std::vector<QVariant>& data, Table *table): _table(table)
 {
     _data = std::vector<QVariant>(data.begin(), data.end());
 }
 
 Record::Record(const Record &data)
 {
+    _table = data._table;
     _data = std::vector<QVariant>(data.cbegin(), data.cend());
 }
 
@@ -30,6 +37,16 @@ void Record::changed(bool yesno)
 bool Record::isValid() const
 {
     return _data.size() != 0;
+}
+
+ColumnDefinition Record::columnDefinition(const quint32 idx) const
+{
+    _table->columndefinition(idx);
+}
+
+ColumnDefinition Record::columnDefinition(const QString name) const
+{
+    _table->columndefinition(name);
 }
 
 quint64 Record::itemid() const
@@ -62,6 +79,12 @@ QVariant Record::cell(quint32 column) const
 
 void Record::cell(quint32 column, const QVariant &value)
 {
+    ColumnDefinition colDef = columnDefinition(column);
+    if (colDef.isReadOnly()) {
+        WARN1("Cannot change readonly cell of '%1'.", colDef.name());
+        return;
+    }
+
     if ( column < _data.size()){
         _data.at(column) = value;
         _changed = true;
