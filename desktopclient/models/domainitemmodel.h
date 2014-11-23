403a5c74dc37a6cd1b6db85eaa46b1ffe3bfd901
Martin Schouwenburg <mschouwen@gmail.com>
2014-11-23 22:38:35 +0100

    new

---
+++ desktopclient/models/domainitemmodel.h
@@ -0,0 +1,37 @@
+#ifndef DomainItemModel_H
+#define DomainItemModel_H
+
+#include <QObject>
+
+#include "kernel.h"
+#include "ilwisdata.h"
+#include "range.h"
+#include "domain.h"
+#include "domainitem.h"
+#include "itemdomain.h"
+#include "identifieritem.h"
+#include "thematicitem.h"
+#include "identifierrange.h"
+
+class DomainItemModel : public QObject
+{
+    Q_OBJECT
+public:
+    DomainItemModel();
+    explicit DomainItemModel(Ilwis::SPItemRange _range, const QString &name, QObject *parent = 0);
+
+    QString name() const;
+    QString code() const;
+    QString description() const;
+
+signals:
+
+public slots:
+
+private:
+    Ilwis::SPItemRange _range;
+    QString _itemname;
+
+};
+
+#endif // DomainItemModel_H
---
+++ desktopclient/qml/Global.js
@@ -0,0 +1,6 @@
+.pragma library
+var mainbackgroundcolor = "white";
+var edgecolor = "lightgrey"
+var alternatecolor1 = "lightgrey"
+var alternatecolor2 = "white"
+var alternatecolor3 = "#F7F9FC"
