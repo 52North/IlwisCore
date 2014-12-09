b642e03a5d29fb604f9d4199db1879328f228798
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2014-12-09 15:55:52 +0100

    new

---
+++ geodrawer/tesselation/ilwistesselator.h
@@ -0,0 +1,26 @@
+#ifndef ILWISTESSELATOR_H
+#define ILWISTESSELATOR_H
+
+#include "tesselator.h"
+
+namespace Ilwis {
+namespace Geodrawer{
+
+class IlwisTesselator
+{
+public:
+    IlwisTesselator();
+    ~IlwisTesselator();
+    void tesselate(const ICoordinateSystem& csyRoot, const ICoordinateSystem& csyGeom,const geos::geom::Geometry *geometry, std::vector<VertexPosition> &points, std::vector<VertexIndex> &indices);
+
+private:
+   std::vector<std::vector<float>> getContours(const geos::geom::Geometry *geometry,const ICoordinateSystem &csyRoot, const ICoordinateSystem &csyGeom);
+   void tesselateInternal(const std::vector<std::vector<float>>& contours,std::vector<VertexPosition> &points, std::vector<VertexIndex> &indices);
+
+    TESStesselator *_tessaltor = 0;
+    TESSalloc _ma;
+};
+}
+}
+
+#endif // ILWISTESSELATOR_H
