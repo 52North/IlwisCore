7723355d3d0467ae2d07807b5aa56df13f3b05ea
Martin Schouwenburg <mschouwen@gmail.com>
2014-12-18 23:05:47 +0100

    new

--- core/ilwisobjects/representation/palettecolorlookup.h
+++ core/ilwisobjects/representation/palettecolorlookup.h
@@ -1,10 +1,19 @@
 #ifndef PALETTECOLORLOOKUP_H
 #define PALETTECOLORLOOKUP_H
 
-class PaletteColorLookup : public ColorLookup
-{
+#include "colorlookup.h"
+#include "boost/container/flat_map.hpp"
+
+namespace Ilwis {
+class KERNELSHARED_EXPORT PaletteColorLookUp : public ColorLookUp{
+
 public:
-    PaletteColorLookup();
+    PaletteColorLookUp(boost::container::flat_map<quint32, QColor>& newcolors);
+    QColor value2color(double index, const NumericRange& actualRange = NumericRange(), const NumericRange& stretchRange = NumericRange()) const;
+
+private:
+    boost::container::flat_map<quint32, QColor> _colors;
 };
+}
 
 #endif // PALETTECOLORLOOKUP_H
