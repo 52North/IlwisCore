e098d670ac5b588214929f12c71d66af8203307b
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-01-07 15:22:59 +0100

    reworked to be the container for the layers in the visualization

--- ilwiscoreui/models/visualizationmanager.h
+++ ilwiscoreui/models/visualizationmanager.h
@@ -7,27 +7,33 @@
 #include "objectvisualizationmodel.h"
 
 class ObjectVisualizationModel;
-
+class UIContextModel;
 
 class ILWISCOREUISHARED_EXPORT VisualizationManager : public QObject
 {
     Q_OBJECT
 
+    Q_PROPERTY(QQmlListProperty<ObjectVisualizationModel> layers READ layers NOTIFY layerChanged)
+
 public:
     explicit VisualizationManager(QObject *parent = 0);
+    VisualizationManager(QObject *parent, UIContextModel *context);
 
     void addVisualizationModel(ObjectVisualizationModel *newmodel);
-
+    Q_INVOKABLE void addDataSource(const QString& url, const QString& typeName);
+    QQmlListProperty<ObjectVisualizationModel> layers();
     Q_INVOKABLE ObjectVisualizationModel *layer(quint32 layerIndex);
 
 signals:
     void removeLayer(const Ilwis::Resource& resource);
+    void layerChanged();
 
 public slots:
 
 private:
 
     QList<ObjectVisualizationModel *> _layers;
+    UIContextModel *_uicontext;
     void init();
 
 };
