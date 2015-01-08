9f52744df711ea7a2fe7323f99cf1a147615555e
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-01-08 14:23:33 +0100

    qmlUrl method now returns a string instead of a url as qml doesnt understand urls (by default)

--- ilwiscoreui/models/propertyeditormetadata.h
+++ ilwiscoreui/models/propertyeditormetadata.h
@@ -10,15 +10,15 @@ class ILWISCOREUISHARED_EXPORT PropertyEditorMetaData : public QObject, public I
 {
     Q_OBJECT
     Q_PROPERTY(QString editorName READ editorName CONSTANT)
+    Q_PROPERTY(QString qmlUrl READ qmlUrl CONSTANT)
 public:
     PropertyEditorMetaData(QObject *parent = 0);
     explicit PropertyEditorMetaData(const QString& name, const QUrl& Url, QObject *parent = 0);
     PropertyEditorMetaData(const PropertyEditorMetaData& metadata, QObject *parent);
 
-    QUrl qmlUrl() const;
-
 private:
     QString editorName() const;
+    QString qmlUrl() const;
     QUrl _qmlUrl;
 signals:
 
