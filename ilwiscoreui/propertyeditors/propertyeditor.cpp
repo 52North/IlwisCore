0e30015063b61c7a0108c4729144b73ee95b43dc
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-01-15 15:50:02 +0100

    merge of old class and propertymetadata

--- ilwiscoreui/propertyeditors/propertyeditor.cpp
+++ ilwiscoreui/propertyeditors/propertyeditor.cpp
@@ -1,6 +1,38 @@
 #include "propertyeditor.h"
 
-PropertyEditor::PropertyEditor(QObject *parent) :
-    QObject(parent)
+PropertyEditor::PropertyEditor(QObject *parent) : QObject(parent)
 {
+
 }
+
+PropertyEditor::PropertyEditor(const QString& name, const QUrl &url, QObject *parent) : QObject(parent), Ilwis::Identity(name), _qmlUrl(url)
+{
+
+}
+
+PropertyEditor::PropertyEditor(const PropertyEditor& metadata, QObject *parent) : QObject(parent), Ilwis::Identity(metadata.name())
+{
+    _qmlUrl = metadata._qmlUrl;
+}
+
+QString PropertyEditor::qmlUrl() const
+{
+    return _qmlUrl.toString();
+}
+
+QString PropertyEditor::editorName() const
+{
+    return name();
+}
+
+void PropertyEditor::layer(CoverageLayerModel *model){
+    _layer = model;
+}
+
+CoverageLayerModel *PropertyEditor::layer()
+{
+    return _layer;
+}
+
+
+
--- ilwiscoreui/propertyeditors/propertyeditor.h
+++ ilwiscoreui/propertyeditors/propertyeditor.h
@@ -1,18 +1,42 @@
-#ifndef PROPERTYEDITOR_H
-#define PROPERTYEDITOR_H
+#ifndef PROPERTYEDITOR2_H
+#define PROPERTYEDITOR2_H
 
 #include <QObject>
+#include <QUrl>
+#include "identity.h"
+#include "uicontextmodel.h"
+#include "ilwiscoreui_global.h"
 
-class PropertyEditor : public QObject
+class CoverageLayerModel;
+
+class ILWISCOREUISHARED_EXPORT PropertyEditor : public QObject, public Ilwis::Identity
 {
     Q_OBJECT
+    Q_PROPERTY(QString editorName READ editorName CONSTANT)
+    Q_PROPERTY(QString qmlUrl READ qmlUrl CONSTANT)
 public:
-    explicit PropertyEditor(QObject *parent = 0);
+    PropertyEditor(QObject *parent = 0);
+    explicit PropertyEditor(const QString& name, const QUrl& url, QObject *parent = 0);
+    PropertyEditor(const PropertyEditor& metadata, QObject *parent);
+    QString editorName() const;
 
+    void layer(CoverageLayerModel *model);
+protected:
+    CoverageLayerModel* layer();
+    QUrl _qmlUrl;
+private:
+    CoverageLayerModel *_layer = 0;
+    QString qmlUrl() const;
 signals:
 
 public slots:
-
 };
 
-#endif // PROPERTYEDITOR_H
+#define NEW_PROPERTYEDITOR(name) \
+    private: \
+static int dummy_propertyeditor;
+
+#define REGISTER_PROPERTYEDITOR(typeint, propertyname, name) \
+    int name::dummy_propertyeditor = uicontext()->addPropertyEditor(typeint, propertyname, name::create);
+
+#endif // PROPERTYEDITOR2_H
