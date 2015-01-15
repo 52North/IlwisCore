8db4670731e14fa19f44d173bf4ffba91d20a045
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-01-15 15:51:24 +0100

    partial type definitions are now sufficien t to recreate a propertyeditir

--- ilwiscoreui/models/uicontextmodel.cpp
+++ ilwiscoreui/models/uicontextmodel.cpp
@@ -24,13 +24,14 @@ QString UIContextModel::uniqueName()
     return "ilwis_ui_object_" + QString::number(_objectCounter++);
 }
 
-QList<PropertyEditorMetaData *> UIContextModel::propertyEditors(quint64 objecttype)
+QList<PropertyEditor *> UIContextModel::propertyEditors(quint64 objecttype)
 {
-    QList<PropertyEditorMetaData *> editors;
-    auto iter = _propertyEditors.find(objecttype);
-    if ( iter != _propertyEditors.end()){
-        for(const auto &editor : (*iter).second)    {
-            editors.append(editor.second);
+    QList<PropertyEditor *> editors;
+    for ( const auto& ilwtype : _propertyEditors){
+        if ( hasType(ilwtype.first, objecttype)){
+            for(auto editor : _propertyEditors[ilwtype.first]) {
+                editors.append(editor.second());
+            }
         }
     }
     return editors;
@@ -53,10 +54,14 @@ void UIContextModel::setActiveSplit(int index)
     }
 }
 
-void UIContextModel::addPropertyEditor(quint64 objecttype, const QString &propertyName, const PropertyEditorMetaData& metadata)
+int UIContextModel::addPropertyEditor(quint64 objecttype, const QString &propertyName, CreatePropertyEditor func)
 {
-    _propertyEditors[objecttype][propertyName] = new PropertyEditorMetaData(metadata, this);
+    _propertyEditors[objecttype][propertyName] = func;
+
+    return 0;
 }
+
+
 
 
 
