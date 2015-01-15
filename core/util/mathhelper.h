1c0ba02df6a15e7a6b75b9ee867e1a798db842ff
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-01-15 15:49:40 +0100

    added method to calculate nice ranges

--- core/util/mathhelper.h
+++ core/util/mathhelper.h
@@ -3,17 +3,18 @@
 
 namespace Ilwis {
 
-struct Coefficient{
+struct KERNELSHARED_EXPORT Coefficient{
     Coefficient(double x=0, double y=0) : _x(x), _y(y){}
     double _x,_y;
 };
 
-class MathHelper
+class KERNELSHARED_EXPORT MathHelper
 {
 public:
     MathHelper();
     static bool findOblique(int iPoints, const std::vector<Coordinate>& independent, const std::vector<Coordinate>& dependent, std::vector<Coefficient>& coef, bool useCols);
     static bool findPolynom(int iTerms, int iPoints, const std::vector<Coordinate>& independent, const std::vector<Coordinate>& dependent, std::vector<Coefficient>& coef);
+    static NumericRange roundRange(double rmin, double rmax, double step);
 };
 }
 #endif // MATHHELPER_H
