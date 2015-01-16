d10d56189bdab2c2125f1bb61a7b9f53a4b68927
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-01-16 14:18:27 +0100

    moved from geodrawer

---
+++ ilwiscoreui/drawers/drawerinterface.cpp
@@ -0,0 +1,12 @@
+#include "drawerinterface.h"
+
+
+Ilwis::Geodrawer::DrawerInterface::DrawerInterface(QObject *parent) : QObject(parent)
+{
+
+}
+
+Ilwis::Geodrawer::DrawerInterface::~DrawerInterface()
+{
+
+}
