58c66664e9edbf8f79c34728762ae6682a7cffa0
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-01-27 15:45:16 +0100

    value2color now works (no skeletal implementation anymore)

--- core/ilwisobjects/representation/palettecolorlookup.h
+++ core/ilwisobjects/representation/palettecolorlookup.h
@@ -8,11 +8,14 @@ namespace Ilwis {
 class KERNELSHARED_EXPORT PaletteColorLookUp : public ColorLookUp{
 
 public:
+    PaletteColorLookUp(const QString &definition);
     PaletteColorLookUp(boost::container::flat_map<quint32, QColor>& newcolors);
     QColor value2color(double index, const NumericRange& actualRange = NumericRange(), const NumericRange& stretchRange = NumericRange()) const;
 
 private:
     boost::container::flat_map<quint32, QColor> _colors;
+    bool _cyclic = true;
+    int _cycleCount = 16;
 };
 }
 
