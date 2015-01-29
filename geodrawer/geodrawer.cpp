ca8f916737c668b7f1b540cc55ba802f7e1b2d18
Martin Schouwenburg <mschouwen@gmail.com>
2015-01-29 23:16:02 +0100

    added tr/catch

--- geodrawer/geodrawer.cpp
+++ geodrawer/geodrawer.cpp
@@ -20,52 +20,58 @@ GeoDrawer::GeoDrawer(QQuickItem *parent):
 
 void GeoDrawer::addDataSource(const QString &url, const QString& typeName, VisualizationManager* manager)
 {
-    if ( url == "" || typeName == "")
-        return;
-
-    if ( !_rootDrawer)
-         _rootDrawer = new Ilwis::Geodrawer::RootDrawer(this);
-
-    IlwisTypes tp = Ilwis::IlwisObject::name2Type(typeName);
-    if ( tp == itUNKNOWN)
-        return;
-
-    for(ResourceModel *resource : _datasources)    {
-        if ( resource->url() == url && resource->type() == tp)
+    try {
+        if ( url == "" || typeName == "")
             return;
-    }
 
-    quint64 id = mastercatalog()->url2id(QUrl(url),tp);
-    if ( id == i64UNDEF){
-        ERROR2(ERR_COULDNT_CREATE_OBJECT_FOR_2, TR("Visualization"), url);
-        return ;
-    }
-    Resource resource = mastercatalog()->id2Resource(id);
-    if (! resource.isValid()){
-        ERROR2(ERR_COULDNT_CREATE_OBJECT_FOR_2, TR("Visualization"), url);
-        return ;
-    }
+        if ( !_rootDrawer)
+            _rootDrawer = new Ilwis::Geodrawer::RootDrawer(this);
 
-    _datasources.push_back(new ResourceModel(resource,this));
-    ICoverage coverage(resource);
-    if ( !coverage.isValid()){
-        ERROR2(ERR_COULDNT_CREATE_OBJECT_FOR_2, TR("Visualization"), url);
-        return;
-    }
-    Geodrawer::LayerDrawer *drawer = Geodrawer::DrawerFactory::create<Geodrawer::LayerDrawer>(coverage->ilwisType(), _rootDrawer, _rootDrawer);
-    if ( !drawer)
-        return;
+        IlwisTypes tp = Ilwis::IlwisObject::name2Type(typeName);
+        if ( tp == itUNKNOWN)
+            return;
 
-    coverage->loadData(); // loaddata now so that all attribute info( ranges) is known; we need to load the data anyway.
-    drawer->coverage(coverage);
-    Size<> sz(width(), height(), 1);
-    _rootDrawer->pixelAreaSize(sz);
-    if ( _rootDrawer->drawerCount(Geodrawer::ComplexDrawer::dtMAIN) == 0)
-        _rootDrawer->coordinateSystem(coverage->coordinateSystem());
-    _rootDrawer->addDrawer(drawer,false);
+        for(ResourceModel *resource : _datasources)    {
+            if ( resource->url() == url && resource->type() == tp)
+                return;
+        }
 
-    if ( manager){
-        manager->addDataSource(url, typeName, drawer);
+        quint64 id = mastercatalog()->url2id(QUrl(url),tp);
+        if ( id == i64UNDEF){
+            ERROR2(ERR_COULDNT_CREATE_OBJECT_FOR_2, TR("Visualization"), url);
+            return ;
+        }
+        Resource resource = mastercatalog()->id2Resource(id);
+        if (! resource.isValid()){
+            ERROR2(ERR_COULDNT_CREATE_OBJECT_FOR_2, TR("Visualization"), url);
+            return ;
+        }
+
+        _datasources.push_back(new ResourceModel(resource,this));
+        ICoverage coverage(resource);
+        if ( !coverage.isValid()){
+            ERROR2(ERR_COULDNT_CREATE_OBJECT_FOR_2, TR("Visualization"), url);
+            return;
+        }
+        Geodrawer::LayerDrawer *drawer = Geodrawer::DrawerFactory::create<Geodrawer::LayerDrawer>(coverage->ilwisType(), _rootDrawer, _rootDrawer);
+        if ( !drawer)
+            return;
+
+        coverage->loadData(); // loaddata now so that all attribute info( ranges) is known; we need to load the data anyway.
+        drawer->coverage(coverage);
+        Size<> sz(width(), height(), 1);
+        _rootDrawer->pixelAreaSize(sz);
+        if ( _rootDrawer->drawerCount(Geodrawer::ComplexDrawer::dtMAIN) == 0)
+            _rootDrawer->coordinateSystem(coverage->coordinateSystem());
+        _rootDrawer->addDrawer(drawer,false);
+
+        if ( manager){
+            manager->addDataSource(url, typeName, drawer);
+        }
+    } catch ( const ErrorObject& err){
+
+    } catch ( const std::exception& ex){
+        kernel()->issues()->log(ex.what());
     }
 }
 
