2a24fc7bde27c0561db8665b959f41cc9b4428fa
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-02-04 15:26:27 +0100

    added new files

--- geodrawer/geodrawer_plugin.cpp
+++ geodrawer/geodrawer_plugin.cpp
@@ -3,10 +3,19 @@
 
 #include <qqml.h>
 
+#include "geometries.h"
+#include "iooptions.h"
+#include "drawerfactory.h"
+#include "selectiondrawer.h"
+
 void GeodrawerPlugin::registerTypes(const char *uri)
 {
     // @uri n52.org.ilwisobjects
     qmlRegisterType<GeoDrawer>(uri, 1, 0, "GeoDrawer");
+
+    Ilwis::Geodrawer::DrawerFactory::registerDrawer("SelectionDrawer", Ilwis::Geodrawer::SelectionDrawer::create);
+
+
 }
 
 
