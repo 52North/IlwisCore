576a62104a68647fb4ccbf346256713329f29ff2
Martin Schouwenburg <mschouwen@gmail.com>
2015-03-08 20:00:49 +0100

    editors are based on thr actual ilwisobject and not on its type. now the canUse can check the properties of an object to see which editors are appropriate

--- ilwiscoreui/models/uicontextmodel.cpp
+++ ilwiscoreui/models/uicontextmodel.cpp
@@ -24,15 +24,16 @@ QString UIContextModel::uniqueName()
     return "ilwis_ui_object_" + QString::number(_objectCounter++);
 }
 
-QList<PropertyEditor *> UIContextModel::propertyEditors(quint64 objecttype)
+QList<PropertyEditor *> UIContextModel::propertyEditors(const Ilwis::IIlwisObject& obj)
 {
     QList<PropertyEditor *> editors;
-    for ( const auto& ilwtype : _propertyEditors){
-        if ( hasType(ilwtype.first, objecttype)){
-            for(auto editor : _propertyEditors[ilwtype.first]) {
-                editors.append(editor.second());
-            }
-        }
+    for ( const auto& editorItem : _propertyEditors){
+        auto *editor = editorItem.second();
+        if ( editor && editor->canUse(obj)){
+                editor->prepare(obj);
+                editors.append(editor);
+        }else
+            delete editor;
     }
     return editors;
 }
@@ -77,9 +78,9 @@ int UIContextModel::currentKey() const
     return _currentKey;
 }
 
-int UIContextModel::addPropertyEditor(quint64 objecttype, const QString &propertyName, CreatePropertyEditor func)
+int UIContextModel::addPropertyEditor(const QString &propertyName, CreatePropertyEditor func)
 {
-    _propertyEditors[objecttype][propertyName] = func;
+    _propertyEditors[propertyName] = func;
 
     return 0;
 }
