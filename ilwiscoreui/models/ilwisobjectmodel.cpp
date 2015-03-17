a56c63c5e61b1ccb9e495a8cdec2a8497c134b30
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-03-17 15:40:22 +0100

    changed the default name for the global layer

--- ilwiscoreui/models/ilwisobjectmodel.cpp
+++ ilwiscoreui/models/ilwisobjectmodel.cpp
@@ -18,7 +18,7 @@ IlwisObjectModel::IlwisObjectModel()
 IlwisObjectModel::IlwisObjectModel(const Ilwis::Resource &source, QObject *parent) : ResourceModel(source, parent)
 {
     try{
-        if ( source.name() != "Global Property Editors"){ // special case for the dummy object of the global layer
+        if ( source.name() != "Global Layer"){ // special case for the dummy object of the global layer
             _ilwisobject.prepare(resource());
         }
     } catch (const ErrorObject& ){
