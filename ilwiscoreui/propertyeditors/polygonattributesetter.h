e4b291be77395d660a101818a44ea79181b7e018
Martin Schouwenburg <mschouwen@gmail.com>
2015-03-29 23:07:42 +0200

    filled the skeleton with the working code

--- ilwiscoreui/propertyeditors/polygonattributesetter.h
+++ ilwiscoreui/propertyeditors/polygonattributesetter.h
@@ -7,13 +7,28 @@
 
 class PolygonPropertySetter : public VisualAttributeEditor
 {
+    Q_OBJECT
+
+    Q_PROPERTY(bool showBoundaries READ showBoundaries WRITE setShowBoundaries NOTIFY showBoundariesChanged)
+    Q_PROPERTY(bool showAreas READ showAreas WRITE setShowAreas NOTIFY showAreasChanged)
 public:
     ~PolygonPropertySetter();
     PolygonPropertySetter(QObject *parent=0);
-    bool canUse(const IIlwisObject &obj, const ColumnDefinition &) const;
     static VisualAttributeEditor *create();
 
-  NEW_PROPERTYEDITOR(PolygonPropertySetter)
+    NEW_PROPERTYEDITOR(PolygonPropertySetter)
+    bool canUse(const IIlwisObject &obj, const QString &name) const;
+    bool showBoundaries() const;
+    void setShowBoundaries(bool yesno);
+    bool showAreas() const;
+    void setShowAreas(bool yesno);
+
+signals:
+    void showBoundariesChanged();
+    void showAreasChanged();
+
+private:
+
 };
 
 #endif // POLYGONPROPERTYSETTER_H
