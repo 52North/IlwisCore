dfc391c1470e93781065474de5e2f91a7ca823d2
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-03-30 15:50:08 +0200

    changed some statements that were apparently illegal with a newer driver

--- core/resources/featurevertexshader_nvdia.glsl
+++ core/resources/featurevertexshader_nvdia.glsl
@@ -6,17 +6,21 @@ uniform float scalefactor;
 uniform float alpha;
 attribute lowp vec4 vertexColor;
 varying lowp vec4 fragmentColor;
+
 void main() {
+vec4 pos;
+vec4 vColor = vertexColor;
+pos = position;
 if ( scalefactor != 1) {
 	 float x = scalecenter[0] + (position[0] - scalecenter[0]) * scalefactor;
 	 float y = scalecenter[1] + (position[1] - scalecenter[1]) * scalefactor;
 	 float z = position[2];
-	position[0] = x;
-	position[1] = y;
-	position[2] = z;
-	position[3] = 1;
+	pos[0] = x;
+	pos[1] = y;
+	pos[2] = z;
+	pos[3] = 1;
  }
- vertexColor.a = alpha * vertexColor.a;
- gl_Position =  mvp * position;
- fragmentColor = vertexColor;
+ vColor.a = alpha * vertexColor.a;
+ gl_Position =  mvp * pos;
+ fragmentColor = vColor;
 }
