cea3b7c6de38cc3be8ec5a7b37a8968ec893cda0
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-04-02 15:23:34 +0200

    wip

--- geodrawer/rasterimage.cpp
+++ geodrawer/rasterimage.cpp
@@ -1,11 +1,15 @@
 #include <QImage>
 #include "raster.h"
+#include "pixeliterator.h"
+#include "rootdrawer.h"
+#include "drawers/attributevisualproperties.h"
 #include "rasterimage.h"
 
 using namespace Ilwis;
 using namespace Geodrawer;
 
-RasterImage::RasterImage()
+RasterImage::RasterImage(RootDrawer *rootDrawer, const IRasterCoverage& raster, const VisualAttribute& vattribute) :
+    _raster(raster), _rootDrawer(rootDrawer), _visualAttribute(vattribute)
 {
 
 }
@@ -15,9 +19,46 @@ RasterImage::~RasterImage()
 
 }
 
-
-
-void Ilwis::Geodrawer::RasterImage::setSelection(const BoundingBox &box)
+const std::unique_ptr<QImage> &RasterImage::image()
 {
-
+    return _image;
 }
+
+
+void RasterImage::makeImage()
+{
+    Size<> rastersize = _raster->size();
+    BoundingBox bb = _rootDrawer->screenGrf()->coord2Pixel(_raster->envelope());
+    quint32 size = bb.xlength() * bb.ylength();
+    _pixels.resize(size);
+    PixelIterator pixIter(_raster);
+     PixelIterator pixIter2(_raster);
+     auto end2 = pixIter.end();
+     int xblockSize = bb.xlength() / rastersize.xsize();
+     int yblockSize = bb.ylength() / rastersize.ysize();
+     int indexBase = 0;
+     int lastXBlockStart = 0;
+     while(pixIter2 != end2){
+         double value = *pixIter2;
+         auto color = _visualAttribute.value2color(value);
+         for(int y = 0; y < yblockSize; ++y){
+             for(int x = 0; x < xblockSize; ++x){
+                 _pixels[indexBase + x] = color;
+                 ++indexBase;
+             }
+             indexBase += bb.xlength() - xblockSize;
+         }
+         ++pixIter2;
+         if ( pixIter2.ychanged()){
+            indexBase = indexBase + xblockSize + 1;
+         }else
+            indexBase = lastXBlockStart + xblockSize;
+         lastXBlockStart = indexBase;
+
+     }
+     const uchar *datablock = (const uchar *)_pixels.data();
+    _image.reset(new QImage(datablock,bb.xlength(), bb.ylength(),QImage::Format_RGBA8888));
+}
+
+
+
--- geodrawer/rasterimage.h
+++ geodrawer/rasterimage.h
@@ -1,20 +1,25 @@
 #ifndef RASTERIMAGE_H
 #define RASTERIMAGE_H
+class QImage;
 
 namespace Ilwis{
 namespace Geodrawer {
+class RootDrawer;
 
-class RasterImage : public QImage
+class RasterImage
 {
 public:
-    RasterImage();
-    RasterImage(const IRasterCoverage& raster);
+    RasterImage(RootDrawer *rootDrawer, const IRasterCoverage& raster, const VisualAttribute &vattribute);
     ~RasterImage();
 
-    void setSelection(const BoundingBox& box);
-
+    const std::unique_ptr<QImage>& image();
+    void makeImage();
 private:
     IRasterCoverage _raster;
+    RootDrawer *_rootDrawer;
+    VisualAttribute _visualAttribute;
+    std::unique_ptr<QImage> _image;
+    std::vector<VertexColorI> _pixels;
 
 
 };
