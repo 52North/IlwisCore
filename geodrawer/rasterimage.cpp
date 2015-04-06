4c2795e8c746ac5c018915455b4dd45854c86fdc
Martin Schouwenburg <mschouwen@gmail.com>
2015-04-06 22:03:33 +0200

    first working version; some work needs to be done though for efficiency and correct coordinate use

--- geodrawer/rasterimage.cpp
+++ geodrawer/rasterimage.cpp
@@ -27,36 +27,21 @@ const std::unique_ptr<QImage> &RasterImage::image()
 
 void RasterImage::makeImage()
 {
-    Size<> rastersize = _raster->size();
-    BoundingBox bb = _rootDrawer->screenGrf()->coord2Pixel(_raster->envelope());
+    BoundingBox bb(_raster->size()); // = _rootDrawer->screenGrf()->coord2Pixel(_raster->envelope());
     quint32 size = bb.xlength() * bb.ylength();
     _pixels.resize(size);
     PixelIterator pixIter(_raster);
-     PixelIterator pixIter2(_raster);
-     auto end2 = pixIter.end();
-     int xblockSize = bb.xlength() / rastersize.xsize();
-     int yblockSize = bb.ylength() / rastersize.ysize();
-     int indexBase = 0;
-     int lastXBlockStart = 0;
-     while(pixIter2 != end2){
-         double value = *pixIter2;
-         auto color = _visualAttribute.value2color(value);
-         for(int y = 0; y < yblockSize; ++y){
-             for(int x = 0; x < xblockSize; ++x){
-                 _pixels[indexBase + x] = color;
-                 ++indexBase;
-             }
-             indexBase += bb.xlength() - xblockSize;
-         }
-         ++pixIter2;
-         if ( pixIter2.ychanged()){
-            indexBase = indexBase + xblockSize + 1;
-         }else
-            indexBase = lastXBlockStart + xblockSize;
-         lastXBlockStart = indexBase;
 
-     }
-     const uchar *datablock = (const uchar *)_pixels.data();
+    auto end = pixIter.end();
+    while(pixIter != end){
+        double value = *pixIter;
+        auto color = _visualAttribute.value2color(value);
+        _pixels[pixIter.linearPosition()] = color;
+        ++pixIter;
+
+    }
+
+    const uchar *datablock = (const uchar *)_pixels.data();
     _image.reset(new QImage(datablock,bb.xlength(), bb.ylength(),QImage::Format_RGBA8888));
 }
 
