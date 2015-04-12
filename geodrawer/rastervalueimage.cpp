8b86bffa6cd2ac71973c6d9362e994647ebd3a2a
Martin Schouwenburg <mschouwen@gmail.com>
2015-04-12 18:30:59 +0200

    new

--- geodrawer/rastervalueimage.cpp
+++ geodrawer/rastervalueimage.cpp
@@ -1,12 +1,63 @@
+#include <QImage>
+#include "raster.h"
+#include "pixeliterator.h"
+#include "rootdrawer.h"
+#include "drawers/attributevisualproperties.h"
 #include "rastervalueimage.h"
 
-RasterValueImage::RasterValueImage()
-{
+using namespace Ilwis;
+using namespace Geodrawer;
 
+RasterValueImage::RasterValueImage(RootDrawer *rootDrawer, const IRasterCoverage& raster, const VisualAttribute &vattribute) :
+    RasterImage(rootDrawer,raster,vattribute)
+{
 }
 
 RasterValueImage::~RasterValueImage()
 {
 
 }
+
+bool RasterValueImage::prepare(int prepareType)
+{
+    bool resetImage = false;
+    BoundingBox bb(_raster->size());
+    if ( hasType(prepareType, DrawerInterface::ptRENDER)){
+        std::vector<QColor> colors = _visualAttribute.colors();
+        _colorTable = QVector<QRgb>(colors.size()) ;
+        for(int i = 0; i < _colorTable.size(); ++i)
+            _colorTable[i] = colors[i].rgba();
+        resetImage = true;
+
+    }
+
+    if ( hasType(prepareType, DrawerInterface::ptGEOMETRY)){
+        BoundingBox bb(_raster->size());
+        quint32 size = bb.xlength() * bb.ylength();
+        _pixels.resize(size);
+        PixelIterator pixIter(_raster);
+
+
+
+
+        SPNumericRange numrange =  _raster->datadef().range<NumericRange>();
+        auto end = pixIter.end();
+        while(pixIter != end){
+            double value = *pixIter;
+            int index = value == rUNDEF ? 0 : 1 + (_colorTable.size() - 1) * (value - numrange->min()) / numrange->distance();
+            _pixels[pixIter.linearPosition()] = index;
+            ++pixIter;
+        }
+        resetImage = true;
+    }
+
+    if ( resetImage)   {
+        const uchar *datablock = (const uchar *)_pixels.data();
+        _image.reset(new QImage(datablock,bb.xlength(), bb.ylength(),QImage::Format_Indexed8));
+        _image->setColorTable(_colorTable);
+    }
+    return resetImage;
+
+}
+
 
--- geodrawer/rastervalueimage.h
+++ geodrawer/rastervalueimage.h
@@ -1,12 +1,23 @@
 #ifndef RASTERVALUEIMAGE_H
 #define RASTERVALUEIMAGE_H
 
+#include "rasterimage.h"
+
+namespace Ilwis{
+namespace Geodrawer {
 
 class RasterValueImage : public RasterImage
 {
 public:
-    RasterValueImage();
+    RasterValueImage(Ilwis::Geodrawer::RootDrawer *rootDrawer, const Ilwis::IRasterCoverage &raster, const Ilwis::Geodrawer::VisualAttribute &vattribute);
     ~RasterValueImage();
+
+    bool prepare(int prepareType);
+private:
+    QVector<QRgb> _colorTable;
+    std::vector<quint8> _pixels;
 };
+}
+}
 
 #endif // RASTERVALUEIMAGE_H
