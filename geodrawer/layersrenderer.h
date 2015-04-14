a302cc2560a8d386ee48307518a72019dcb4fa65
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-04-14 09:04:28 +0200

    emits signal when synchronize is done

--- geodrawer/layersrenderer.h
+++ geodrawer/layersrenderer.h
@@ -81,6 +81,8 @@ public:
     void synchronize(QQuickFramebufferObject *item);
 
     void cleanup();
+signals:
+    void synchronizeDone();
 
 private:
     Ilwis::Geodrawer::RootDrawer *_rootDrawer = 0;
