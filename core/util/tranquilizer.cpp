da3f3d897e5ec987fbc71ea749efdd736fced7a4
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-04-23 11:25:55 +0200

    now functional

--- core/util/tranquilizer.cpp
+++ core/util/tranquilizer.cpp
@@ -1,3 +1,5 @@
+#include <QThread>
+#include <QCoreApplication>
 #include "kernel.h"
 #include "tranquilizer.h"
 
@@ -6,24 +8,27 @@ using namespace Ilwis;
 quint64 Tranquilizer::_trqId = 0;
 
 Tranquilizer::Tranquilizer(QObject *parent) :
-    QObject(parent)
+    QObject(parent), _id(i64UNDEF),_start(0),_end(0), _current(0)
 {
 
 }
 
-Tranquilizer::Tranquilizer(const QString& title, const QString& description, double end) :  QObject(0),
-    _title(title), _desc(description), _end(end)
+Tranquilizer::Tranquilizer(const QString& title, const QString& description, double end, double start) :  QObject(0)
 {
-    _id = _trqId++;
+    prepare(title, description,end,start);
 }
 
-void Tranquilizer::prepare(const QString &title, const QString &description, double end)
+void Tranquilizer::prepare(const QString &title, const QString &description, double end, double start)
 {
     _title = title;
     _desc = description;
     _end = end;
-    kernel()->connect(this, &Tranquilizer::updateTranquilizer, kernel(), &Kernel::changeTranquilizer,Qt::DirectConnection);
-    kernel()->newTranquilizer(_id, title, description, _end);
+    _start = start;
+    _current = _start;
+    _id = _trqId++;
+    kernel()->connect(this, &Tranquilizer::updateTranquilizer, kernel(), &Kernel::changeTranquilizer);
+    kernel()->connect(this, &Tranquilizer::removeTranquilizer, kernel(), &Kernel::removeTranquilizer);
+    kernel()->newTranquilizer(_id, title, description, _start, _end);
 
 }
 
@@ -37,6 +42,12 @@ double Tranquilizer::end() const
     return _end;
 }
 
+void Tranquilizer::stop()
+{
+    Locker<std::mutex> lock(_mutex);
+    emit(removeTranquilizer(_id));
+}
+
 double Tranquilizer::current() const
 {
     return _current;
@@ -45,4 +56,37 @@ double Tranquilizer::current() const
 void Tranquilizer::current(double cur)
 {
     _current = cur;
+}
+
+void Tranquilizer::update(double step) {
+    Locker<std::mutex> lock(_mutex);
+    _current += step;
+    if ( _current >= _end || _current < _start){
+        emit(updateTranquilizer(_id, _current));
+        std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+        emit(removeTranquilizer(_id));
+    }else
+        emit(updateTranquilizer(_id, _current));
+
+}
+
+//-------------------------------------------------------------
+TranquilizerWorker::TranquilizerWorker()
+{
+
+}
+
+void TranquilizerWorker::updateTranquilizer(quint64 id, double amount)
+{
+    emit sendUpdateTranquilizer(id, amount);
+}
+
+void TranquilizerWorker::createTranquilizer(quint64 id, const QString &title, const QString &description, double start, double end)
+{
+    emit sendCreateTranquilizer(id, title, description, start, end);
+}
+
+void TranquilizerWorker::removeTranquilizer(quint64 id)
+{
+    emit sendRemoveTranquilizer(id);
 }
--- core/util/tranquilizer.h
+++ core/util/tranquilizer.h
@@ -6,37 +6,53 @@
 #include "locker.h"
 
 namespace Ilwis {
+
+class KERNELSHARED_EXPORT TranquilizerWorker : public QObject {
+    Q_OBJECT
+
+public:
+    TranquilizerWorker();
+public slots:
+
+    void updateTranquilizer(quint64 id, double amount);
+    void createTranquilizer(quint64 id, const QString &title, const QString &description, double start, double end);
+    void removeTranquilizer(quint64 id);
+signals:
+    void sendUpdateTranquilizer(quint64 id, double amount);
+    void sendCreateTranquilizer(quint64 id,const QString &title, const QString &description, double start, double end);
+    void sendRemoveTranquilizer(quint64 id);
+    //void sendtranquilizersChanged();
+};
+
+
 class KERNELSHARED_EXPORT Tranquilizer : public QObject
 {
     Q_OBJECT
 public:
     explicit Tranquilizer(QObject *parent = 0);
-    Tranquilizer(const QString &title, const QString &description, double end);
-    void prepare(const QString &title, const QString &description, double end);
+    Tranquilizer(const QString &title, const QString &description, double end,double start=0);
+    void prepare(const QString &title, const QString &description, double end, double start=0);
 
 
-    void update(double step) {
-        Locker<std::mutex> lock(_mutex);
-        _current += step;
-        emit(updateTranquilizer(_id, _current));
-
-    }
-
+    void update(double step);
     double current() const;
     void current(double cur);
     void end(double number);
     double end() const;
+    void stop();
 
 private:
     static quint64 _trqId;
     quint64 _id;
     QString _title;
     QString _desc;
+    double _start;
     double  _end;
     double _current;
     std::mutex _mutex;
 signals:
     void updateTranquilizer(quint64 id, double current);
+    void removeTranquilizer(quint64 id);
     void tranquilizerCreated(quint64 id, const QString &title, const QString &description, quint64 end);
     
 public slots:
