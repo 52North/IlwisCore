281c1400e36b890831100559760853254d19064c
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-06-24 09:39:06 +0200

    result of the operation will be stored in the variable "result" and can be emitted to the system

--- ilwiscoreui/models/operationworker.cpp
+++ ilwiscoreui/models/operationworker.cpp
@@ -13,33 +13,34 @@ OperationWorker::OperationWorker(const OperationExpression &opExpr) :_expression
 }
 
 void OperationWorker::process(){
-    Operation op(_expression);
-    SymbolTable tbl;
-    ExecutionContext ctx;
+    try {
+        Operation op(_expression);
+        SymbolTable tbl;
+        ExecutionContext ctx;
 
-    if(op->execute(&ctx, tbl)){
-        if ( ctx._results.size() > 0){
-            for(auto resultName : ctx._results){
-                Symbol symbol = tbl.getSymbol(resultName);
-                if ( hasType(symbol._type, itNUMBER)){
-                    result += symbol._var.toDouble();
-                }else if ( hasType(symbol._type, itSTRING)){
-                    result += symbol._var.toString();
-                }else if ( hasType(symbol._type, (itCOVERAGE | itTABLE))){
-                    if ( symbol._type == itRASTER){
-                        IRasterCoverage raster = symbol._var.value<IRasterCoverage>();
-                        QString path = context()->persistentInternalCatalog().toString() + "/" + raster->name() + ".ilwis";
-                        raster->connectTo(path,
-                                          "rastercoverage",
-                                          "stream",
-                                          IlwisObject::cmOUTPUT);
-                        raster->store();
+        if(op->execute(&ctx, tbl)){
+            if ( ctx._results.size() > 0){
+                for(auto resultName : ctx._results){
+                    Symbol symbol = tbl.getSymbol(resultName);
+                    if ( hasType(symbol._type, itNUMBER)){
+                        result += symbol._var.toDouble();
+                    }else if ( hasType(symbol._type, itSTRING)){
+                        result += symbol._var.toString();
+                    }else if ( hasType(symbol._type, (itCOVERAGE | itTABLE))){
+                        if ( symbol._type == itRASTER){
+                            IRasterCoverage raster = symbol._var.value<IRasterCoverage>();
+                            if ( raster.isValid())
+                                result = raster->source().url().toString();
+                        }
                     }
                 }
             }
+        }else {
+            qDebug() << "operation failed";
         }
-    }else {
-        qDebug() << "operation failed";
+        emit finished();
+    }catch(const ErrorObject& err){
+
     }
     emit finished();
 }
