fbfab9519ec04992a10781d97a0ded0bb3385473
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-07-01 14:37:08 +0200

    split up between tables and rasters/numbers. tables have a different syntax (with selectors) for expressions. it was too difficult in the UI to detect this apart from splitting the application in two variants. Conceptual its ok as the table variant had a different parameter list anyway (with column name)

--- baseoperations/math/unarymath.h
+++ baseoperations/math/unarymath.h
@@ -13,24 +13,12 @@ public:
     enum UnaryOperations{uoSIN, uoCOS, uoTAN, uoSQRT, uoASIN, uoACOS, uoATAN, uoLog10, uoLN, uoABS, uoCEIL,
                          uoFLOOR,uoCOSH, uoEXP, uoNEG,uoRND,uoSGN,uoSINH,uoTANH};
     UnaryMath();
-    UnaryMath(quint64 metaid, const Ilwis::OperationExpression &expr, const QString& outpDom, UnaryFunction fun);
+    UnaryMath(quint64 metaid, const Ilwis::OperationExpression &expr, const QString& outputdom, UnaryFunction fun);
 
 protected:
-    static Resource populateMetadata(const QString &item, const QString &longname, const QString& outputDom);
-    Ilwis::OperationImplementation::State prepare(Ilwis::ExecutionContext *, const Ilwis::SymbolTable &);
-    bool execute( ExecutionContext *ctx, SymbolTable& symTable);
-
-    IRasterCoverage _inputGC;
-    IRasterCoverage _outputGC;
-    ITable _inputTable;
-    ITable _outputTable;
-    QString _outColumn;
-    QString _inColumn;
-    BoundingBox _box;
     OperationType _case;
-    double _number;
-    QString _outputDomain;
     UnaryFunction _unaryFun;
+    QString _outputDomain;
 
 };
 }
