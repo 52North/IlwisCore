cf80c088f1d505771e7b0525d4094b9184d1aaef
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-07-09 15:03:09 +0200

    the creators are now stored in a creatormodel and this forced some reorganization in this class

--- ilwiscoreui/models/objectcreator.cpp
+++ ilwiscoreui/models/objectcreator.cpp
@@ -1,11 +1,29 @@
 #include "resourcemodel.h"
+#include "ilwisobjectcreatormodel.h"
 #include "objectcreator.h"
 
 using namespace Ilwis;
 
 ObjectCreator::ObjectCreator(QObject *parent) : QObject(parent)
 {
-
+    _creators.append(new IlwisObjectCreatorModel("Numeric Domain",itNUMERICDOMAIN,"CreateNumDom.qml", this));
+    _creators.append(new IlwisObjectCreatorModel("Thematic Domain",itITEMDOMAIN | itTHEMATICITEM,"CreateThematicDom.qml", this));
+    _creators.append(new IlwisObjectCreatorModel("Identifier Domain",itITEMDOMAIN | itIDENTIFIERITEM,"CreateNumDom.qml", this));
+    _creators.append(new IlwisObjectCreatorModel("Indexed Domain",itITEMDOMAIN | itINDEXEDITEM,"CreateNumDom.qml", this));
+    _creators.append(new IlwisObjectCreatorModel("Interval Domain",itITEMDOMAIN | itNUMERICITEM,"CreateNumDom.qml", this));
+    _creators.append(new IlwisObjectCreatorModel("Time Domain",itTIME | itDOMAIN,"CreateNumDom.qml", this));
+    _creators.append(new IlwisObjectCreatorModel("Time Interval Domain",itTIMEITEM | itITEMDOMAIN,"CreateNumDom.qml", this));
+    _creators.append(new IlwisObjectCreatorModel("Color Domain",itCOLORDOMAIN,"CreateNumDom.qml", this));
+    _creators.append(new IlwisObjectCreatorModel("Color Palette Domain",itPALETTECOLOR | itITEMDOMAIN,"CreateNumDom.qml", this));
+    _creators.append(new IlwisObjectCreatorModel("Corners Georeference",itGEOREF,"CreateNumDom.qml", this));
+    _creators.append(new IlwisObjectCreatorModel("Tiepoints Georeference",itGEOREF | itLOCATION,"CreateNumDom.qml", this));
+    _creators.append(new IlwisObjectCreatorModel("Projected Coordinate System",itCONVENTIONALCOORDSYSTEM,"CreateNumDom.qml", this));
+    _creators.append(new IlwisObjectCreatorModel("LatLon Coordinate System",itCONVENTIONALCOORDSYSTEM|itLOCATION,"CreateNumDom.qml", this));
+    _creators.append(new IlwisObjectCreatorModel("Bounds only Coordinate System",itBOUNDSONLYCSY,"CreateNumDom.qml", this));
+    _creators.append(new IlwisObjectCreatorModel("Raster Coverage",itRASTER,"CreateNumDom.qml", this));
+    _creators.append(new IlwisObjectCreatorModel("Feature Coverage",itFEATURE,"CreateNumDom.qml", this));
+    _creators.append(new IlwisObjectCreatorModel("Table",itTABLE,"CreateNumDom.qml", this));
+    _creators.append(new IlwisObjectCreatorModel("Representation",itREPRESENTATION,"CreateNumDom.qml", this));
 }
 
 ObjectCreator::~ObjectCreator()
@@ -16,25 +34,24 @@ ObjectCreator::~ObjectCreator()
 QStringList ObjectCreator::createableObjects() const
 {
     QStringList nameiconpairs;
-    nameiconpairs.append(TR("Numeric Domain") + "|" + ResourceModel::iconPath(itNUMERICDOMAIN));
-    nameiconpairs.append(TR("Thematic Domain") + "|" + ResourceModel::iconPath(itITEMDOMAIN | itTHEMATICITEM));
-    nameiconpairs.append(TR("Identifier Domain") + "|" + ResourceModel::iconPath(itITEMDOMAIN | itIDENTIFIERITEM));
-    nameiconpairs.append(TR("Indexed Domain") + "|" + ResourceModel::iconPath(itITEMDOMAIN | itINDEXEDITEM));
-    nameiconpairs.append(TR("Interval Domain") + "|" + ResourceModel::iconPath(itITEMDOMAIN | itNUMERICITEM));
-    nameiconpairs.append(TR("Time Domain") + "|" + ResourceModel::iconPath(itTIME | itDOMAIN));
-    nameiconpairs.append(TR("Time Interval Domain") + "|" + ResourceModel::iconPath(itTIMEITEM | itITEMDOMAIN));
-    nameiconpairs.append(TR("Color Domain") + "|" + ResourceModel::iconPath(itCOLORDOMAIN));
-    nameiconpairs.append(TR("Color Palette Domain") + "|" + ResourceModel::iconPath(itPALETTECOLOR | itITEMDOMAIN));
-    nameiconpairs.append(TR("Corners Georeference") + "|" + ResourceModel::iconPath(itGEOREF));
-    nameiconpairs.append(TR("Tiepoints Georeference") + "|" + ResourceModel::iconPath(itGEOREF | itLOCATION));
-    nameiconpairs.append(TR("Projected Coordinate System") + "|" + ResourceModel::iconPath(itCONVENTIONALCOORDSYSTEM));
-    nameiconpairs.append(TR("LatLon Coordinate System") + "|" + ResourceModel::iconPath(itCONVENTIONALCOORDSYSTEM|itLOCATION));
-    nameiconpairs.append(TR("Bounds only Coordinate System") + "|" + ResourceModel::iconPath(itBOUNDSONLYCSY));
-    nameiconpairs.append(TR("Raster Coverage") + "|" + ResourceModel::iconPath(itRASTER));
-    nameiconpairs.append(TR("Feature Coverage") + "|" + ResourceModel::iconPath(itFEATURE));
-    nameiconpairs.append(TR("Table") + "|" + ResourceModel::iconPath(itTABLE));
-    nameiconpairs.append(TR("Representation") + "|" + ResourceModel::iconPath(itREPRESENTATION));
+    for(auto *model : _creators)
+        nameiconpairs.append(model->label() + "|" + ResourceModel::iconPath(model->type().toULongLong()));
 
     return nameiconpairs;
 }
+
+void ObjectCreator::setActiveCreator(qint32 index)
+{
+    if ( index >= 0 && index < _creators.size()){
+        _activeCreators.clear();
+        _activeCreators.append(_creators[index]);
+        emit activeCreatorsChanged();
+    }
+}
+
+QQmlListProperty<IlwisObjectCreatorModel> ObjectCreator::activeCreators()
+{
+    return  QQmlListProperty<IlwisObjectCreatorModel>(this, _activeCreators);
+}
+
 
