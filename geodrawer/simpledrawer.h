a1603e493ad2859d9af9370f51007da56567382e
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-08-05 13:18:43 +0200

    added drawerattribute method

--- geodrawer/simpledrawer.h
+++ geodrawer/simpledrawer.h
@@ -14,6 +14,7 @@ class SimpleDrawer : public BaseDrawer
 public:
     SimpleDrawer(const QString &name, DrawerInterface* parentDrawer, RootDrawer *rootdrawer, const IOOptions &options);
     QVariant attributeOfDrawer(const QString& drawercode, const QString& attrName) const;
+    bool drawerAttribute(const QString& , const QString &, const QVariant &);
 
     bool isSimple() const ;
 
