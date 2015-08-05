814c522773ac8abd2617b4682e53bb8ce3a73772
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-08-05 13:11:49 +0200

    added a factory method for cvreating color models

--- core/ilwisobjects/representation/colorlookup.h
+++ core/ilwisobjects/representation/colorlookup.h
@@ -3,13 +3,20 @@
 
 namespace Ilwis {
 
+class Domain;
+typedef IlwisData<Domain> IDomain;
+
+
 class NumericRange;
 
 class KERNELSHARED_EXPORT ColorLookUp
 {
 public:
     virtual QColor value2color(double value, const NumericRange& actualRange = NumericRange(), const NumericRange& stretchRange = NumericRange()) const = 0;
+    virtual void setColor(double value, const QColor& clr)  = 0;
     std::vector<QColor> values2colors(const NumericRange &actualRange, const NumericRange &stretchRange, int paletteSize=256) const;
+    virtual ColorLookUp *clone() const = 0;
+    static ColorLookUp *create(const Ilwis::IDomain &dom, const QString &rprCode="");
 protected:
     QColor string2color(const QString &colorstring);
 };
