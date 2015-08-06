98ce09c29354d05952bb3097f6fbc48e83fede38
Henning Bredel <h.bredel@52north.org>
2015-08-06 09:05:20 +0200

    integrate boost bgl

--- core/ilwisobjects/workflow/workflow.cpp
+++ core/ilwisobjects/workflow/workflow.cpp
@@ -1,28 +1,84 @@
-
 #include "kernel.h"
 #include "ilwisdata.h"
-#include "operationmetadata.h"
 
 #include "workflow.h"
 
 using namespace Ilwis;
 
-Workflow::Workflow() : OperationMetaData()
+Workflow::Workflow(): OperationMetaData()
 {
+
 }
 
 Workflow::Workflow(const Resource &resource): OperationMetaData(resource)
 {
+}
+
+Workflow::~Workflow() {
 
 }
 
-Workflow::~Workflow()
+OVertex Workflow::addOperation(OperationProperties properties)
 {
-
+    return boost::add_vertex(properties, _wfGraph);
 }
 
+void Workflow::removeOperation(OVertex vertex)
+{
+    boost::clear_vertex(vertex, _wfGraph);
+    boost::remove_vertex(vertex, _wfGraph);
+}
+
+OEdge Workflow::addOperationFlow(OVertex v1, OVertex v2, FlowProperties properties)
+{
+    return (boost::add_edge(v1, v2, properties, _wfGraph)).first;
+}
 
 IlwisTypes Workflow::ilwisType() const
 {
     return itOPERATIONMETADATA | itWORKFLOW;
+}
+
+quint64 Workflow::createMetadata()
+{
+    QString workflowName = name();
+
+
+    /*
+    if (_operationNodes.size() == 1) {
+        OperationMetaData *metadata = _operationNodes.at(0);
+        QString syntax = metadata->source()["syntax"];
+        QString keywords = metadata->source()["keywords"];
+        QString inparameters = metadata->source()["inparameters"];
+        QString outparameters = metadata->source()["outparameters"];
+
+        return mastercatalog()->addItems({operation});
+    }
+
+    // TODO perform actions on graph, i.e. topological sort, etc.
+
+    QList<OperationMetaData*> operationNodes;
+    for (auto it : operationNodes) {
+        OperationMetaData *metadata = it;
+        //metadata->
+    }
+    */
+
+    /*
+    OperationResource operation({"ilwis://operations/aggregateraster"});
+    operation.setLongName("Spatial Aggregation of Raster coverage");
+
+    operation.setSyntax("aggregateraster(inputgridcoverage,!Avg|Max|Med|Min|Prd|Std|Sum, groupsize,changegeometry)");
+
+
+    operation.setDescription(TR("generates a rastercoverage according to a aggregation method. The aggregation method determines how pixel values are used in the aggregation"));
+    operation.setInParameterCount({4});
+    operation.addInParameter(0,itRASTER , TR("input rastercoverage"),TR("input rastercoverage with any domain"));
+    operation.addInParameter(1,itSTRING , TR("Aggregation Method"),TR("the method how pixels inside a group will be accumulated"));
+    operation.addInParameter(2,itINTEGER | itSTRING , TR("Groupsize"),TR("The size of the block used to aggregate. In the case of integer it is a square 2D block; in the case of string it is of the list format (2 or 3 dimensions). eg {3 4}"));
+    operation.addInParameter(3,itBOOL , TR("change geometry"),TR("The aggregation can either create a map with a reduced size proportional to de block size or use the same geometry size but fill all pixels in the block with the aggregate"));
+    operation.setOutParameterCount({1});
+    operation.addOutParameter(0,itRASTER, TR("Aggregated raster"), TR("output rastercoverage with the domain of the input map"));
+    operation.setKeywords("aggregate,raster,geometry");
+     */
 }
--- core/ilwisobjects/workflow/workflow.h
+++ core/ilwisobjects/workflow/workflow.h
@@ -1,28 +1,60 @@
 #ifndef WORKFLOW_H
 #define WORKFLOW_H
 
-#include "boost/graph/graph_traits.hpp"
-#include "boost/graph/adjacency_list.hpp"
+#include <QObject>
+
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
 
 #include "kernel_global.h"
-
-using namespace boost;
-typedef adjacency_list<vecS, vecS, directedS> ExecutionBranch;
+#include "operationmetadata.h"
 
 namespace Ilwis {
 
+struct OperationProperties {
+    quint64 ilwisId;
+};
+
+struct FlowProperties {
+    QString foo = "bar";
+};
+
+typedef boost::property<boost::vertex_index1_t, OperationProperties> NodeProperty;
+typedef boost::property<boost::edge_index_t, FlowProperties> FlowProperty;
+typedef boost::adjacency_list<boost::vecS, boost::vecS,
+                              boost::bidirectionalS,
+                              NodeProperty, FlowProperty> WorkflowGraph;
+
+typedef boost::property_map<WorkflowGraph, boost::vertex_index1_t>::type NodePropertyMap;
+typedef boost::property_map<WorkflowGraph, boost::edge_index_t>::type EdgePropertyMap;
+
+typedef boost::graph_traits<WorkflowGraph>::vertex_descriptor OVertex;
+typedef boost::graph_traits<WorkflowGraph>::edge_descriptor OEdge;
+
+
 class KERNELSHARED_EXPORT Workflow: public OperationMetaData
 {
-public:
+
+private:
     Workflow();
+
+public:
     Workflow(const Resource &resource);
     ~Workflow();
 
+    // ------ workflow API functions
+    OVertex addOperation(OperationProperties opProperties);
+    void removeOperation(OVertex vertex);
+    OEdge addOperationFlow(OVertex v1, OVertex v2, FlowProperties flowProperties);
+    void removeOperationFlow(OEdge edge);
+
+    // ------ operation metadata functions
     IlwisTypes ilwisType() const;
-    static quint64 createMetadata();
+    quint64 createMetadata();
 
 private:
-    QList<OperationMetaData> _operationNodes;
+    WorkflowGraph _wfGraph;
 };
 
 typedef IlwisData<Workflow> IWorkflow;
