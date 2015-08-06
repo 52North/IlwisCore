934dce8cdd5abc7fc3e870072dd345eac451f4d5
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-08-06 15:07:31 +0200

    added the height for each creator that is in the list. This is different for each editor

--- ilwiscoreui/models/ilwisobjectcreatormodel.h
+++ ilwiscoreui/models/ilwisobjectcreatormodel.h
@@ -11,14 +11,16 @@ class ILWISCOREUISHARED_EXPORT IlwisObjectCreatorModel : public QObject
     Q_PROPERTY(QString label READ label CONSTANT)
     Q_PROPERTY(QString type READ type CONSTANT)
     Q_PROPERTY(QString componentUrl READ componentUrl CONSTANT)
+    Q_PROPERTY(int itemHeight READ height CONSTANT)
 public:
     explicit IlwisObjectCreatorModel(QObject *parent = 0);
-    IlwisObjectCreatorModel(const QString& label, quint64 type, const QString& creatorUrl,QObject *parent);
+    IlwisObjectCreatorModel(const QString& label, quint64 type, const QString& creatorUrl, int height, QObject *parent);
     ~IlwisObjectCreatorModel();
 
     QString label() const;
     QString componentUrl() const;
     QString type() const;
+    int height() const;
 
 signals:
 
@@ -28,6 +30,7 @@ private:
     QString _label;
     quint64 _type;
     QString _creatorUrl;
+    int _height;
 };
 
 #endif // IWISOBJECTCREATORMODEL_H
