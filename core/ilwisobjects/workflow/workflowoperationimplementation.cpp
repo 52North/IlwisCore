03bc496e2917ab57c4786585482f94ac85342557
Henning Bredel <h.bredel@52north.org>
2015-08-17 16:59:47 +0200

    add first preparation stuff, prepare to start execution on workflow

--- core/ilwisobjects/workflow/workflowoperationimplementation.cpp
+++ core/ilwisobjects/workflow/workflowoperationimplementation.cpp
@@ -7,6 +7,7 @@
 #include "operation.h"
 
 #include "workflowoperationimplementation.h"
+#include "workflow.h"
 
 using namespace Ilwis;
 
@@ -22,11 +23,45 @@ Ilwis::OperationImplementation *WorkflowOperationImplementation::create(quint64 
 
 Ilwis::OperationImplementation::State WorkflowOperationImplementation::prepare(ExecutionContext *ctx, const SymbolTable &)
 {
+    if (_metadata->ilwisType() != itWORKFLOW) {
+        ERROR0("Expected type itWORKFLOW here!");
+        return sPREPAREFAILED;
+    }
 
+    Workflow *workflow = static_cast<Workflow*>(_metadata.ptr());
+
+    QStringList required;
+    QStringList optionals;
+    workflow->parametersFromSyntax(required, optionals);
+
+    if (_expression.parameterCount() < required.size()) {
+        QString list = required.join(", ");
+        ERROR1("Number of required paramaters do not match! Required are: %2", list);
+        return sPREPAREFAILED;
+    }
+    std::vector<SPOperationParameter> inputs = workflow->getInputParameters();
+    std::vector<SPOperationParameter> outputs = workflow->getOutputParameters();
+
+    // TODO do more preparation and validation checks
+
+    _prepState = sPREPARED;
+    return _prepState;
 }
 
 bool WorkflowOperationImplementation::execute(ExecutionContext *ctx, SymbolTable &symTable)
 {
+    if (_prepState == sNOTPREPARED)
+        if((_prepState = prepare(ctx,symTable)) != sPREPARED)
+            return false;
+
+    Workflow *workflow = static_cast<Workflow*>(_metadata.ptr());
+    QList<OVertex> inputNodes = workflow->getNodesWithExternalInput();
+
+    // execution traversal over edges
+
+    QList<OVertex> outputNodes = workflow->getNodesWithExternalOutputs();
+
     // TODO
+
     return true;
 }
