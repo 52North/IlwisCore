f0bbdf90b122dbf048850b1edf16b8764a4987ae
Henning Bredel <h.bredel@52north.org>
2015-08-18 16:33:09 +0200

    initiate first workflow execution code

--- core/ilwisobjects/workflow/workflowoperationimplementation.cpp
+++ core/ilwisobjects/workflow/workflowoperationimplementation.cpp
@@ -7,7 +7,6 @@
 #include "operation.h"
 
 #include "workflowoperationimplementation.h"
-#include "workflow.h"
 
 using namespace Ilwis;
 
@@ -48,14 +47,43 @@ Ilwis::OperationImplementation::State WorkflowOperationImplementation::prepare(E
     return _prepState;
 }
 
+
 bool WorkflowOperationImplementation::execute(ExecutionContext *ctx, SymbolTable &symTable)
 {
     if (_prepState == sNOTPREPARED)
         if((_prepState = prepare(ctx,symTable)) != sPREPARED)
             return false;
 
-    Workflow *workflow = static_cast<Workflow*>(_metadata.ptr());
+    IWorkflow workflow = (IWorkflow)_metadata;
     QList<OVertex> inputNodes = workflow->getNodesWithExternalInput();
+
+    quint16 inputParamIndex = 0;
+    for (OVertex inputNode : inputNodes) {
+        QStringList requireds;
+        QStringList optionals;
+        IOperationMetaData meta = workflow->getOperationMetadata(inputNode);
+        meta->parametersFromSyntax(requireds,optionals);
+
+        QStringList arguments;
+        for (int i = 0 ; i < requireds.size() ; i++) {
+            arguments << _expression.parm(inputParamIndex).value();
+            inputParamIndex++;
+        }
+        for (int i = 0 ; i < optionals.size() ; i++) {
+            if (inputParamIndex < _expression.parameterCount()) {
+                arguments << _expression.parm(inputParamIndex).value();
+                inputParamIndex++;
+            }
+        }
+
+        _inputArgs.insert(inputNode, arguments);
+    }
+
+
+    if ( !inputNodes.isEmpty()) {
+        executeFromInputNode(inputNodes.at(0), ctx, symTable);
+    }
+
 
     // execution traversal over edges
 
@@ -64,4 +92,41 @@ bool WorkflowOperationImplementation::execute(ExecutionContext *ctx, SymbolTable
     // TODO
 
     return true;
+}
+
+
+bool WorkflowOperationImplementation::executeFromInputNode(const OVertex &v, ExecutionContext *ctx, SymbolTable &symTable)
+{
+    QStringList requireds;
+    QStringList optionals;
+    IWorkflow workflow = (IWorkflow)_metadata;
+    IOperationMetaData meta = workflow->getOperationMetadata(v);
+    meta->parametersFromSyntax(requireds,optionals);
+
+    QStringList outputParameters;
+    for (int i = 0 ; i < meta->getOutputParameters().size() ; i++) {
+        SPOperationParameter parameter = meta->getOutputParameters().at(i);
+        QString outputterm = parameter->term().isEmpty()
+                ? QString("pout_%1_%2").arg(v).arg(i)
+                : parameter->term() ;
+        outputParameters << outputterm;
+    }
+
+
+    QString execString = QString("%1%2(%3)"); // TODO named arguments
+    if ( !outputParameters.isEmpty()) {
+        execString = execString.arg(outputParameters.join(", ") + "=");
+    }
+    execString = execString.arg(meta->name());
+    execString = execString.arg(_inputArgs[v].join(", "));
+
+    qDebug() << "execute node " << v << " via: " << execString;
+
+    bool ok = commandhandler()->execute(execString, ctx, symTable);
+    if ( !ok) {
+        ERROR1("workflow execution failed when executing: ", execString);
+    }
+
+
+
 }
--- core/ilwisobjects/workflow/workflowoperationimplementation.h
+++ core/ilwisobjects/workflow/workflowoperationimplementation.h
@@ -1,6 +1,8 @@
 #ifndef WORKFLOWOPERATIONIMPLEMENTATION_H
 #define WORKFLOWOPERATIONIMPLEMENTATION_H
 
+#include "workflow.h"
+
 namespace Ilwis {
 class WorkflowOperationImplementation: public OperationImplementation
 {
@@ -9,10 +11,13 @@ public:
 
     bool execute(ExecutionContext *ctx,SymbolTable& symTable);
     static Ilwis::OperationImplementation *create(quint64 metaid,const Ilwis::OperationExpression& expr);
-    Ilwis::OperationImplementation::State prepare(ExecutionContext *ctx, const SymbolTable &);
+    Ilwis::OperationImplementation::State prepare(Ilwis::ExecutionContext *ctx, const SymbolTable &);
 
 protected:
 
+private:
+    QMap<OVertex, QStringList> _inputArgs;
+    bool executeFromInputNode(const OVertex &v, ExecutionContext *ctx, SymbolTable &symTable);
 
 
 };
