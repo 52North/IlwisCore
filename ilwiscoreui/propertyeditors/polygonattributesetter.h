2f4ae47ed4a707141c8bf65c2ee569d112cdc7e9
MartinSchouwenburg <didymothichus1352>
2015-09-27 19:53:36 +0200

    moved line stuff to seperate editor

--- ilwiscoreui/propertyeditors/polygonattributesetter.h
+++ ilwiscoreui/propertyeditors/polygonattributesetter.h
@@ -9,10 +9,8 @@ class PolygonPropertySetter : public VisualAttributeEditor
 {
     Q_OBJECT
 
-    Q_PROPERTY(bool showBoundaries READ showBoundaries WRITE setShowBoundaries NOTIFY showBoundariesChanged)
     Q_PROPERTY(bool showAreas READ showAreas WRITE setShowAreas NOTIFY showAreasChanged)
-    Q_PROPERTY(QColor boundaryColor READ boundaryColor WRITE setBoundaryColor NOTIFY boundaryColorChanged)
-    Q_PROPERTY(int boundarywidth READ boundarywidth WRITE setBoundarywidth NOTIFY boundarywidthChanged)
+
 public:
     ~PolygonPropertySetter();
     PolygonPropertySetter(QObject *parent=0);
@@ -20,21 +18,13 @@ public:
 
     NEW_PROPERTYEDITOR(PolygonPropertySetter)
     bool canUse(const IIlwisObject &obj, const QString &name) const;
-    bool showBoundaries() const;
-    void setShowBoundaries(bool yesno);
     bool showAreas() const;
     void setShowAreas(bool yesno);
-    QColor boundaryColor() const;
-    void setBoundaryColor(const QColor& clr);
-    float boundarywidth() const;
-    void setBoundarywidth(float w);
+
 
 
 signals:
-    void showBoundariesChanged();
     void showAreasChanged();
-    void boundaryColorChanged();
-    void boundarywidthChanged();
 
 private:
 
