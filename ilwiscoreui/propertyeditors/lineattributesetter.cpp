25faae315aca061e9913b64dcc3b37cdb86dff0f
MartinSchouwenburg <didymothichus1352>
2015-09-27 19:53:09 +0200

    was a skeleton implementation. is now functional

--- ilwiscoreui/propertyeditors/lineattributesetter.cpp
+++ ilwiscoreui/propertyeditors/lineattributesetter.cpp
@@ -9,6 +9,7 @@
 #include "feature.h"
 #include "table.h"
 #include "lineattributesetter.h"
+#include "models/visualattributemodel.h"
 
 REGISTER_PROPERTYEDITOR("linepropertysetter",LinePropertySetter)
 
@@ -22,14 +23,14 @@ LinePropertySetter::~LinePropertySetter()
 
 }
 
-bool LinePropertySetter::canUse(const IIlwisObject &obj, const ColumnDefinition& ) const
+bool LinePropertySetter::canUse(const IIlwisObject &obj, const QString& name) const
 {
     if ( !obj.isValid())
         return false;
 
     if ( hasType(obj->ilwisType(), itFEATURE)){
         IFeatureCoverage features = obj.as<FeatureCoverage>();
-        return features->featureCount(itLINE) > 0;
+        return (name == VisualAttributeModel::LAYER_ONLY) && (features->featureCount(itLINE) || features->featureCount(itPOLYGON)> 0);
     }
     return false;
 }
@@ -38,3 +39,70 @@ VisualAttributeEditor *LinePropertySetter::create()
 {
     return new LinePropertySetter();
 }
+
+bool LinePropertySetter::showBoundaries() const
+{
+    if ( !attribute()->layer())
+        return false;
+
+    QVariant var = attribute()->layer()->drawer()->attribute("polygonboundaries");
+    if ( var.isValid())
+        return var.toBool();
+    return false;
+
+}
+
+void LinePropertySetter::setShowBoundaries(bool yesno)
+{
+    if ( !attribute()->layer())
+        return ;
+
+    attribute()->layer()->drawer()->setAttribute("polygonboundaries", yesno);
+    attribute()->layer()->drawer()->redraw();
+
+}
+
+void LinePropertySetter::setBoundaryColor(const QColor &clr)
+{
+    if ( !attribute()->layer())
+        return ;
+
+    attribute()->layer()->drawer()->setAttribute("boundarycolor", clr);
+    attribute()->layer()->drawer()->unprepare(Ilwis::Geodrawer::DrawerInterface::ptRENDER);
+    attribute()->layer()->drawer()->prepare(Ilwis::Geodrawer::DrawerInterface::ptRENDER, Ilwis::IOOptions("polygononly",true));
+    attribute()->layer()->drawer()->redraw();
+}
+
+QColor LinePropertySetter::boundaryColor() const
+{
+    if ( attribute()->layer()){
+        QVariant var = attribute()->layer()->drawer()->attribute("boundarycolor");
+        if ( var.isValid())
+            return var.value<QColor>();
+    }
+    return QColor();
+
+}
+
+float LinePropertySetter::boundarywidth() const
+{
+    if ( !attribute()->layer())
+        return 1.0;
+    QVariant var = attribute()->layer()->drawer()->attribute("boundarywidth");
+    if ( var.isValid())
+        return var.toInt();
+    return 1.0;
+
+}
+
+void LinePropertySetter::setBoundarywidth(float w)
+{
+    if ( !attribute()->layer())
+        return ;
+
+    attribute()->layer()->drawer()->setAttribute("boundarywidth", w);
+    attribute()->layer()->drawer()->unprepare(Ilwis::Geodrawer::DrawerInterface::ptRENDER);
+    attribute()->layer()->drawer()->prepare(Ilwis::Geodrawer::DrawerInterface::ptRENDER, Ilwis::IOOptions("polygononly",true));
+    attribute()->layer()->drawer()->redraw();
+}
+
