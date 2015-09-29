5538d0b59630a56a37344cbfcda3f9d470743020
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-09-29 15:05:59 +0200

    included identifier domains in creatiomn

--- ilwiscoreui/models/objectcreator.cpp
+++ ilwiscoreui/models/objectcreator.cpp
@@ -14,7 +14,7 @@ ObjectCreator::ObjectCreator(QObject *parent) : QObject(parent)
     _creators.append(new IlwisObjectCreatorModel("Workflow",itWORKFLOW,"CreateWorkflow.qml", 400, this));
     _creators.append(new IlwisObjectCreatorModel("Numeric Domain",itNUMERICDOMAIN,"CreateNumDom.qml", 250, this));
     _creators.append(new IlwisObjectCreatorModel("Thematic Domain",itITEMDOMAIN | itTHEMATICITEM,"CreateThematicDom.qml", 520, this));
-    _creators.append(new IlwisObjectCreatorModel("Identifier Domain",itITEMDOMAIN | itIDENTIFIERITEM,"CreateNumDom.qml", 200, this));
+    _creators.append(new IlwisObjectCreatorModel("Identifier Domain",itITEMDOMAIN | itIDENTIFIERITEM,"CreateIdentifierDomain.qml", 520, this));
     _creators.append(new IlwisObjectCreatorModel("Indexed Domain",itITEMDOMAIN | itINDEXEDITEM,"CreateNumDom.qml", 200, this));
     _creators.append(new IlwisObjectCreatorModel("Interval Domain",itITEMDOMAIN | itNUMERICITEM,"CreateNumDom.qml", 200, this));
     _creators.append(new IlwisObjectCreatorModel("Time Domain",itTIME | itDOMAIN,"CreateNumDom.qml", 200, this));
@@ -60,28 +60,44 @@ void ObjectCreator::setActiveCreator(qint32 index)
 }
 
 QString ObjectCreator::createItemDomain(const QVariantMap &parms){
+    QString expression;
     if( parms["valuetype"].toString() == "thematic"){
         if ( parms["name"].toString() == ""){
             kernel()->issues()->log(TR("Domain must have a valid name"));
             return QString::number(i64UNDEF);
         }
-        QString expression = QString("script %1{format(stream,\"domain\")}=createthematicdomain(\"%2\",%3,%4")
+        expression = QString("script %1{format(stream,\"domain\")}=createthematicdomain(\"%2\",%3,%4")
                 .arg(parms["name"].toString())
                 .arg(parms["items"].toString())
                 .arg(parms["strict"].toBool() ? "yes" : "no")
                 .arg(parms["description"].toString());
         if ( parms["parentdomain"].toString() != "")
-                expression += ","+ parms["parentdomain"].toString();
+            expression += ","+ parms["parentdomain"].toString();
         expression += ")";
 
-        Ilwis::ExecutionContext ctx;
-        Ilwis::SymbolTable syms;
-        if(Ilwis::commandhandler()->execute(expression,&ctx,syms) ) {
-            IDomain obj = syms.getSymbol(ctx._results[0])._var.value<IDomain>();
-            return QString::number(obj->id());
-        }
     }
-  return QString::number(i64UNDEF);
+    if( parms["valuetype"].toString() == "identifier"){
+        if ( parms["name"].toString() == ""){
+            kernel()->issues()->log(TR("Domain must have a valid name"));
+            return QString::number(i64UNDEF);
+        }
+        expression = QString("script %1{format(stream,\"domain\")}=createidentifierdomain(\"%2\",%3,%4")
+                .arg(parms["name"].toString())
+                .arg(parms["items"].toString())
+                .arg(parms["strict"].toBool() ? "yes" : "no")
+                .arg(parms["description"].toString());
+        if ( parms["parentdomain"].toString() != "")
+            expression += ","+ parms["parentdomain"].toString();
+        expression += ")";
+    }
+
+    Ilwis::ExecutionContext ctx;
+    Ilwis::SymbolTable syms;
+    if(expression != "" && Ilwis::commandhandler()->execute(expression,&ctx,syms) ) {
+        IDomain obj = syms.getSymbol(ctx._results[0])._var.value<IDomain>();
+        return QString::number(obj->id());
+    }
+    return QString::number(i64UNDEF);
 }
 
 QString ObjectCreator::createNumericDomain(const QVariantMap &parms)
@@ -99,7 +115,7 @@ QString ObjectCreator::createNumericDomain(const QVariantMap &parms)
             .arg(parms["strict"].toBool() ? "yes" : "no")
             .arg(parms["description"].toString());
     if ( parms["parentdomain"].toString() != "")
-            expression += ","+ parms["parentdomain"].toString();
+        expression += ","+ parms["parentdomain"].toString();
     expression += ")";
 
     Ilwis::ExecutionContext ctx;
