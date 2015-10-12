45b05ce88fc5fd02f37cc21a8e2c13fad3ddaee1
retsios <v.retsios@utwente.nl>
2015-10-12 14:59:55 +0200

    added option to transform the texture coordinates this is needed when temporarily displaying textures made for another zoomfactor this is done to at least display something while the user waits for a texture-download and/or computation

--- core/resources/rastervertexshader_nvdia.glsl
+++ core/resources/rastervertexshader_nvdia.glsl
@@ -1,10 +1,11 @@
 attribute highp vec4 position;
 attribute highp vec4 texCoord;
 uniform mediump mat4 mvp;
+uniform mediump mat4 texmat;
 varying highp vec4 texc;
 
 void main( void )
 {
     gl_Position = mvp * position;
-    texc = texCoord;
+    texc = texmat * texCoord;
 }
