fadae3291d4e1a02e1f5462a108daf17b8e9a634
MartinSchouwenburg <didymothichus1352>
2015-10-18 21:40:32 +0200

    added some new deafult filters

--- ilwiscoreui/models/mastercatalogmodel.cpp
+++ ilwiscoreui/models/mastercatalogmodel.cpp
@@ -88,8 +88,10 @@ void MasterCatalogModel::addDefaultFilters(){
     QString filter2 = QString("container='%1'");
     _defaultFilters.append(new CatalogFilterModel(this,filter.arg(QString::number(itUNKNOWN)),"",""));
     _defaultFilters.append(new CatalogFilterModel(this,"","-- System Catalog -----------------------------",""));
-    _defaultFilters.append(new CatalogFilterModel(this,filter2.arg("ilwis://system/domains"),"System Domains",""));
-    _defaultFilters.append(new CatalogFilterModel(this,filter2.arg("ilwis://system/representations"),"System Representations",""));
+    _defaultFilters.append(new CatalogFilterModel(this,filter2.arg("ilwis://system/domains"),"Domains",""));
+    _defaultFilters.append(new CatalogFilterModel(this,filter2.arg("ilwis://system/representations"),"Representations",""));
+    _defaultFilters.append(new CatalogFilterModel(this,filter2.arg("ilwis://system/projections"),"Projections",""));
+    _defaultFilters.append(new CatalogFilterModel(this,filter2.arg("ilwis://system/ellipsoids"),"Ellipsoids",""));
     _defaultFilters.append(new CatalogFilterModel(this,filter2.arg("ilwis://internalcatalog"),"Internal Catalog",""));
     _defaultFilters.append(new CatalogFilterModel(this,"","-- Most recently used --------------------------",""));
 
