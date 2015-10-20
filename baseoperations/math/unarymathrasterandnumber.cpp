59611af9972c6262cf2d83651c322a7cf53052e8
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-10-20 08:54:18 +0200

    the range was alreayd used in previous setter for range so it is in a shared pointer. The range here should then be a clone as it will go to its own shared pointer

--- baseoperations/math/unarymathrasterandnumber.cpp
+++ baseoperations/math/unarymathrasterandnumber.cpp
@@ -116,7 +116,7 @@ OperationImplementation::State UnaryMathRasterAndNumber::prepare(ExecutionContex
          _outputGC->datadefRef().range(newRange);
          for(quint32 i=0; i<_outputGC->size().zsize(); ++i){
                 QString index = _outputGC->stackDefinition().index(i);
-             _outputGC->setBandDefinition(index,{dom, newRange});
+             _outputGC->setBandDefinition(index,{dom, newRange->clone()});
          }
         _case = otSPATIAL;
         return sPREPARED;
