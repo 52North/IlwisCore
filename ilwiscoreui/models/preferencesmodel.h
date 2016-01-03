32f38a37b2c9847d21e9fef1415fcabc1a969fe8
MartinSchouwenburg <didymothichus1352>
2016-01-03 18:53:52 +0100

    new

--- ilwiscoreui/models/preferencesmodel.h
+++ ilwiscoreui/models/preferencesmodel.h
@@ -2,14 +2,28 @@
 #define PREFERENCESMODEL_H
 
 #include <QObject>
+#include "ilwiscoreui_global.h"
 
-class PreferencesModel : public QObject
+class ILWISCOREUISHARED_EXPORT PreferencesModel : public QObject
 {
     Q_OBJECT
+
+    Q_PROPERTY(QString cacheLocation READ cacheLocation WRITE setCacheLocation NOTIFY cacheLocationChanged)
+    Q_PROPERTY(QString internalCatalog READ internalCatalog WRITE setInternalCatalog NOTIFY internalCatalogChanged)
+
 public:
     explicit PreferencesModel(QObject *parent = 0);
 
+private:
+   QString cacheLocation() const;
+   void setCacheLocation(const QString& loc);
+   QString internalCatalog() const;
+   void setInternalCatalog(const QString& loc);
+
 signals:
+    void cacheLocationChanged();
+    void internalCatalogChanged();
+
 
 public slots:
 };
