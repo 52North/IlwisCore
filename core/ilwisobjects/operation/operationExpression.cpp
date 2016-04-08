bc501779a01ad50b2448ea0bb6c7fc500ad95ecc
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2016-04-08 11:48:23 +0200

    received static method which came from operationcatalogmodel to translate the info from an application form into an expression

--- core/ilwisobjects/operation/operationExpression.cpp
+++ core/ilwisobjects/operation/operationExpression.cpp
@@ -1,10 +1,13 @@
 #include <QUrl>
+#include <QStandardPaths>
+#include "dirent.h"
 #include "kernel.h"
 #include "ilwisdata.h"
 #include "domain.h"
 #include "catalog.h"
 #include "connectorinterface.h"
 #include "symboltable.h"
+#include "dataformat.h"
 #include "mastercatalog.h"
 #include "ilwiscontext.h"
 #include "operationExpression.h"
@@ -526,6 +529,251 @@ QString OperationExpression::toString(bool rightsideonly) const
 
     return expression;
 }
+
+QString OperationExpression::modifyTableOutputUrl(const QString& output, const QStringList& parms)
+{
+    QString columnName = output;
+    QString firstTable = parms[0];
+    if ( firstTable.indexOf("://") != -1){
+        int index = firstTable.lastIndexOf("/");
+        firstTable = firstTable.mid(index + 1);
+        index =  firstTable.indexOf(".");
+        if ( index != -1)
+            firstTable = firstTable.left(index) ;
+    }
+    QString internalPath = context()->persistentInternalCatalog().toString();
+    QString outpath = internalPath + "/" + output;
+
+    return outpath;
+
+}
+OperationExpression OperationExpression::createExpression(quint64 operationid, const QString& parameters, bool acceptIncompleteExpressions){
+    if (  parameters == "")
+        return OperationExpression();
+
+    Resource operationresource = mastercatalog()->id2Resource(operationid);
+    if ( !operationresource.isValid())
+        return OperationExpression();
+
+
+
+    QString expression;
+    QStringList parms = parameters.split("|");
+    bool hasMissingParameters = false;
+    int maxinputparameters = operationresource["inparameters"].toString().split("|").last().toInt();
+    for(int i = 0; i < parms.size(); ++ i){
+        if (operationresource.ilwisType() & itWORKFLOW){
+            int parm = i + 1;
+            if (operationresource[QString("pout_%1_optional").arg(parm)] == "false" && i < operationresource["outparameters"].toInt()) {
+                QString value = parms[i + operationresource["inparameters"].toInt()];
+                if (value.split("@@")[0].size() == 0) {
+                    if ( !acceptIncompleteExpressions){
+                        kernel()->issues()->log(TR("Output parameter " + QString::number(i) + " is undefined with name " +  operationresource[QString("pout_%1_name").arg(parm)].toString()));
+                        hasMissingParameters = true;
+                    }
+                }
+            }
+            if (operationresource[QString("pin_%1_optional").arg(parm)] == "false" && i < operationresource["inparameters"].toInt() && parms[i].size() == 0) {
+                if ( !acceptIncompleteExpressions){
+                    kernel()->issues()->log(TR("Input parameter " + QString::number(i) + " is undefined with name " +  operationresource[QString("pin_%1_name").arg(parm)].toString()));
+                    hasMissingParameters = true;
+                }
+            }
+        }
+        if(i < maxinputparameters ){
+            if ( expression.size() != 0)
+                expression += ",";
+            expression += (parms[i] == "" ?  "?" + QString::number(i+1) : parms[i]);
+        }
+
+    }
+
+    if (hasMissingParameters) return OperationExpression();
+
+    QString allOutputsString;
+
+    bool duplicateFileNames = false;
+
+    QStringList parts = operationresource["outparameters"].toString().split("|");
+    int maxparms = parts.last().toInt();
+    int count = 1;
+    for(int i=(parms.size() - maxparms); i<parms.size(); ++i){
+        QString output = parms[i];
+
+
+        QString pout = QString("pout_%1_type").arg(count++);
+
+        IlwisTypes outputtype = operationresource[pout].toULongLong();
+        if ( output.indexOf("@@") != -1 ){
+            QString format;
+            QStringList parts = output.split("@@");
+            output = parts[0];
+            if ( output == ""){
+                if (!acceptIncompleteExpressions)
+                    continue;
+                else
+                    output = "?o" + QString::number(i);
+            }
+
+            //Check if user didnt put the same output name in another output field
+            int occurences = 0;
+            for(int j=(parms.size() - maxparms); j<parms.size(); ++j){
+                QString compareString = parms[j].split("@@")[0];
+                if(output == compareString){
+                    occurences++;
+                }
+            }
+
+            //Add the duplicate name to the list of duplicate names
+            if(occurences>1){
+                duplicateFileNames = true;
+                kernel()->issues()->log(TR("Workflow did not execute, multiple occurences of an output name"));
+            }
+
+            QString formatName = parts[1];
+
+            if ( operationresource.ilwisType() & itWORKFLOW) {
+                QStringList existingFileNames;
+
+                DIR *directory;
+
+                //If not memory
+                QString fileName;
+
+                if(formatName == "Memory" ){
+                    //Get all files in the internal catalog
+                    QString dataLocation = QStandardPaths::writableLocation(QStandardPaths::DataLocation) + "/internalcatalog";
+                    directory = opendir(dataLocation.toStdString().c_str());
+                }else {
+                    //Get all files in the directory
+                    QString dataLocation = output;
+                    dataLocation = QUrl(dataLocation).toLocalFile();
+
+                    QStringList splitUrl = dataLocation.split("/");
+
+                    fileName = splitUrl.last();
+
+                    QString query = "name='" + formatName + "'";
+                    std::multimap<QString, Ilwis::DataFormat>  formats = Ilwis::DataFormat::getSelectedBy(Ilwis::DataFormat::fpNAME, query);
+                    if ( formats.size() == 1){
+                        QString connector = (*formats.begin()).second.property(DataFormat::fpCONNECTOR).toString();
+                        QString code = (*formats.begin()).second.property(DataFormat::fpCODE).toString();
+
+                        QVariantList extensions = Ilwis::DataFormat::getFormatProperties(DataFormat::fpEXTENSION,outputtype, connector, code);
+
+                        fileName += ".";
+                        fileName += extensions[0].toString();
+                    }
+
+                    splitUrl.removeLast();
+
+                    dataLocation = splitUrl.join("/");
+
+                    directory = opendir(dataLocation.toStdString().c_str());
+                }
+
+                struct dirent *file;
+
+                //Put the existing file names in a list for later use
+                while ((file = readdir (directory)) != NULL) {
+                    existingFileNames.push_back(file->d_name);
+                }
+
+                closedir(directory);
+
+                //Check if a file with the same name already exist
+                for(int j=0;j<existingFileNames.size();++j){
+                    if(formatName == "Memory"){
+                        if(existingFileNames[j] == output) {
+                            duplicateFileNames = true;
+                            kernel()->issues()->log(TR("Workflow did not execute duplicate name: " + output + ". Please change this name."));
+                        }
+                    }else{
+                        if(existingFileNames[j] == fileName){
+                            duplicateFileNames = true;
+                            kernel()->issues()->log(TR("Workflow did not execute duplicate name: " + fileName + ". Please change this name."));
+                        }
+                    }
+                }
+            }
+
+            if ( hasType(outputtype, itCOLUMN)){
+                if ( formatName == "Memory"){
+                    output = modifyTableOutputUrl(output, parms);
+                }else
+                    output = parms[0] + "[" + output + "]";
+            }
+            if ( formatName == "Keep original"){
+                IIlwisObject obj;
+                obj.prepare(parms[0], operationresource["pin_1_type"].toULongLong());
+                if ( obj.isValid()){
+                    IlwisTypes type = operationresource[pout].toULongLong();
+                    QVariantList values = DataFormat::getFormatProperties(DataFormat::fpCODE,type,obj->provider());
+                    if ( values.size() != 0){
+                        format = "{format(" + obj->provider() + ",\"" + values[0].toString() + "\")}";
+                    }else{
+                        kernel()->issues()->log(QString("No valid conversion found for provider %1 and format %2").arg(obj->provider()).arg(IlwisObject::type2Name(type)));
+                        return OperationExpression();
+                    }
+                }
+            }
+            //overrule the user if he wants to store things in the internalcatalog, then the format is by defintion stream
+            if ( context()->workingCatalog()->resource().url() == INTERNAL_OBJECT)
+                formatName == "Memory";
+            if ( formatName != "Memory"){ // special case
+                if ( format == "") {
+                    QString query = "name='" + formatName + "'";
+                    std::multimap<QString, Ilwis::DataFormat>  formats = Ilwis::DataFormat::getSelectedBy(Ilwis::DataFormat::fpNAME, query);
+                    if ( formats.size() == 1){
+                        format = "{format(" + (*formats.begin()).second.property(DataFormat::fpCONNECTOR).toString() + ",\"" +
+                                (*formats.begin()).second.property(DataFormat::fpCODE).toString() + "\")}";
+                    }
+                }
+                // if there is no path we extend it with a path unless the output is a new column, output is than the "old" table so no new output object
+                if ( output.indexOf("://") == -1 )
+                    output = context()->workingCatalog()->resource().url().toString() + "/" + output + format;
+                else
+                    output = output + format;
+            }else{
+                if ( hasType(outputtype,itRASTER)){
+                    format = "{format(stream,\"rastercoverage\")}";
+                }else if (hasType(outputtype, itFEATURE)){
+                    format = "{format(stream,\"featurecoverage\")}";
+                }else if (hasType(outputtype, itTABLE | itCOLUMN)){
+                    format = "{format(stream,\"table\")}";
+                }else if (hasType(outputtype, itCATALOG)){
+                    format = "{format(stream,\"catalog\")}";
+                }else if (hasType(outputtype, itDOMAIN)){
+                    format = "{format(stream,\"domain\")}";
+                }else if (hasType(outputtype, itCOORDSYSTEM)){
+                    format = "{format(stream,\"coordinatesystem\")}";
+                }else if (hasType(outputtype, itGEOREF)){
+                    format = "{format(stream,\"georeference\")}";
+                }
+
+                output = output + format;
+            }
+        }
+
+        if(!allOutputsString.isEmpty()){
+            allOutputsString.append(",");
+        }
+        allOutputsString += output;
+    }
+
+    if(!duplicateFileNames){
+        if ( allOutputsString == "")
+            expression = QString("script %1(%2)").arg(operationresource.name()).arg(expression);
+        else
+            expression = QString("script %1=%2(%3)").arg(allOutputsString).arg(operationresource.name()).arg(expression);
+
+        OperationExpression opExpr(expression);
+
+        return opExpr;
+    }
+    return OperationExpression();
+}
+
 
 
 
