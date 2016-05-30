71ad2eeb99173dc155f17b94748e66db3db5ccfc
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2016-05-30 09:59:39 +0200

    added calclatlon functionality

--- core/catalog/catalogexplorerworker.cpp
+++ core/catalog/catalogexplorerworker.cpp
@@ -1,20 +1,34 @@
 #include "kernel.h"
 #include "catalog.h"
 #include "catalogexplorer.h"
+#include "oshelper.h"
 #include "catalogexplorerworker.h"
 
 void CatalogExplorerWorker::process(){
     try{
         for(auto explorer : _explorers){
             std::vector<Ilwis::Resource> items = explorer.first->loadItems(explorer.second);
-            Ilwis::mastercatalog()->addItems(items);
+            Ilwis::mastercatalog()->addItems(items, true);
+        }
+        Ilwis::Resource location;
+        if ( _explorers.size() > 0){
+            location = _explorers[0].first->source();
+        }
+
+        if ( location.isValid() && Ilwis::Catalog::isSpatialDataLocation(location.url())){
+            qDebug() << "scanning " << location.url();
+            QString query = QString("(type & %1) != 0 and container='%2'").arg(QString::number(itCOVERAGE)).
+                    arg(Ilwis::OSHelper::neutralizeFileName(location.url().toString()));
+            calculatelatLonEnvelopes(query, location.name());
+
         }
         emit finished();
-        emit updateContainer();
+       return;
 
     } catch(const Ilwis::ErrorObject& ){
 
     } catch ( const std::exception& ex){
         Ilwis::kernel()->issues()->log(ex.what());
     }
+    emit finished();
 }
