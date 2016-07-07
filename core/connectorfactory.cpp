27ea4b590f837034a85e92139c1b34a7b4646a7b
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2016-07-07 13:19:24 +0200

    added caseinsensitive checking in the filter

--- core/connectorfactory.cpp
+++ core/connectorfactory.cpp
@@ -20,7 +20,7 @@ uint Ilwis::qHash(const ConnectorFilter& filter ){
 bool Ilwis::operator==(const ConnectorFilter& filter1, const ConnectorFilter& filter2 ){
     bool typeOk = (filter1._objectTypes & filter2._objectTypes) != 0;
     //bool providerOk = filter1._provider == filter2._provider;
-    QRegExp regexpr(filter1._provider,Qt::CaseSensitive, QRegExp::Wildcard);
+    QRegExp regexpr(filter1._provider,Qt::CaseInsensitive, QRegExp::Wildcard);
     bool providerOk = regexpr.indexIn(filter2._provider) != -1;
     return  typeOk && providerOk;
 }
