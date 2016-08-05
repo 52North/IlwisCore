2ee022d6402bc3b81aea77a954f9890a38e38aee
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2016-08-05 15:10:14 +0200

    events for keypresses

--- desktopclient/keyfilter.cpp
+++ desktopclient/keyfilter.cpp
@@ -11,12 +11,14 @@ bool KeyFilter::eventFilter(QObject *object, QEvent *event){
     if (event->type() == QEvent::KeyPress) {
         QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
         uicontext()->currentKey(keyEvent->key());
+        emit keyPressed(keyEvent->key());
 
         return false;
     }
 
     if (event->type() == QEvent::KeyRelease) {
         QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
+        emit keyReleased(keyEvent->key());
         uicontext()->currentKey(0);
         return false;
     }
