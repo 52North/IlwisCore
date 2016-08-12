376864469ea018540feca2885fa5e6d8e605738b
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2016-08-12 09:05:05 +0200

    different size types were used when reading/writing the number of items in an interval range

--- core/ilwisobjects/domain/intervalrange.cpp
+++ core/ilwisobjects/domain/intervalrange.cpp
@@ -251,7 +251,7 @@ QString IntervalRange::toString() const
 
 void IntervalRange::store(QDataStream &stream)
 {
-    quint64 size = _items.size();
+    quint32 size = _items.size();
     stream << size;
     for(const auto& item : _items){
         stream << item->raw() << item->name() << item->description();
