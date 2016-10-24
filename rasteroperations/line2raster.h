2c17a569d2601a679bf072d604fb38a339f80909
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2016-10-24 15:37:54 +0200

    new

---
+++ rasteroperations/line2raster.h
@@ -0,0 +1,30 @@
+#ifndef LINE2RASTER_H
+#define LINE2RASTER_H
+
+
+namespace Ilwis {
+namespace RasterOperations {
+
+class Line2Raster : public OperationImplementation
+{
+public:
+    Line2Raster();
+    ~Line2Raster();
+    Line2Raster(quint64 metaid, const Ilwis::OperationExpression &expr);
+
+    bool execute(ExecutionContext *ctx,SymbolTable& symTable);
+    static Ilwis::OperationImplementation *create(quint64 metaid,const Ilwis::OperationExpression& expr);
+    Ilwis::OperationImplementation::State prepare(ExecutionContext *ctx, const SymbolTable &);
+
+    static quint64 createMetadata();
+
+private:
+    IFeatureCoverage _inputfeatures;
+    IRasterCoverage _outputraster;
+    IGeoReference _inputgrf;
+
+    NEW_OPERATION(Line2Raster);
+};
+}
+}
+#endif // LINE2RASTER_H
