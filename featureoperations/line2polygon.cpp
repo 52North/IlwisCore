6e3efc7dca1bcb49e27495a7ef779c44413144dd
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2016-11-04 09:54:47 +0100

    finsied first version

--- featureoperations/line2polygon.cpp
+++ featureoperations/line2polygon.cpp
@@ -4,6 +4,7 @@
 #include "coverage.h"
 #include "geos/geom/Point.h"
 #include "geos/geom/GeometryFactory.h"
+#include "geos/operation/polygonize/Polygonizer.h"
 #include "factory.h"
 #include "abstractfactory.h"
 #include "featurefactory.h"
@@ -35,14 +36,36 @@ Line2Polygon::Line2Polygon()
 
 }
 
-Line2Polygon::Line2Polygon(quint64 metaid, const Ilwis::OperationExpression &expr)
+Line2Polygon::Line2Polygon(quint64 metaid, const Ilwis::OperationExpression &expr) : OperationImplementation(metaid, expr)
 {
 
 }
 
 bool Line2Polygon::execute(ExecutionContext *ctx, SymbolTable &symTable)
 {
-    return false;
+    if (_prepState == sNOTPREPARED)
+        if((_prepState = prepare(ctx,symTable)) != sPREPARED)
+            return false;
+
+    geos::operation::polygonize::Polygonizer polygonizer;
+
+    for(const SPFeatureI& infeature : _inputfeatures){
+        if ( infeature->geometryType() == itLINE){
+            const UPGeometry& geom = infeature->geometry();
+            polygonizer.add(geom.get());
+        }
+    }
+    std::vector<geos::geom::Polygon *> *polygons = polygonizer.getPolygons();
+    for(geos::geom::Polygon *polygon : *polygons){
+        _outputfeatures->newFeature(polygon);
+    }
+    _outputfeatures->setAttributes(_inputfeatures->attributeTable()->copyTable());
+
+    QVariant value;
+    value.setValue<IFeatureCoverage>(_outputfeatures);
+    ctx->setOutput(symTable, value, _outputfeatures->name(), itFEATURE,_outputfeatures->resource());
+
+    return true;
 }
 
 Ilwis::OperationImplementation *Line2Polygon::create(quint64 metaid, const Ilwis::OperationExpression &expr)
@@ -52,10 +75,34 @@ Ilwis::OperationImplementation *Line2Polygon::create(quint64 metaid, const Ilwis
 
 Ilwis::OperationImplementation::State Line2Polygon::prepare(ExecutionContext *ctx, const SymbolTable &)
 {
+    QString features = _expression.parm(0).value();
+    QString outputName = _expression.parm(0,false).value();
+
+    if (!_inputfeatures.prepare(features, itFEATURE)) {
+
+        ERROR2(ERR_COULD_NOT_LOAD_2,features,"");
+        return sPREPAREFAILED;
+    }
+
+    _outputfeatures = OperationHelperFeatures::initialize(_inputfeatures, itFEATURE, itCOORDSYSTEM | itENVELOPE|itDOMAIN);
+    // auto cd = ColumnDefinition("polygons", _inputfeatures->attributeDefinitions().domain());
+   //   _outputfeatures->attributeDefinitionsRef().addColumn(cd);
+
+
     return sPREPARED;
 }
 
 quint64 Line2Polygon::createMetadata()
 {
-    return i64UNDEF;
+    OperationResource operation({"ilwis://operations/line2polygon"});
+    operation.setSyntax("line2polygon(inputfeatures)");
+    operation.setLongName("Lines to Polygon");
+    operation.setDescription(TR("Converts closed rings to polygons and assings the line value to the new polygon"));
+    operation.setInParameterCount({1});
+    operation.addInParameter(0,itFEATURE, TR("input lines"),TR("input feature coverage with lines"));
+    operation.setOutParameterCount({1});
+    operation.addOutParameter(0,itPOLYGON, TR("output polygon coverage"), TR("Collection of polygons"));
+    operation.setKeywords("line,polygon,vector");
+    mastercatalog()->addItems({operation});
+    return operation.id();
 }
