89226f1de30353b2d44ca7075cc4ce1351d34e8a
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2016-11-11 09:07:45 +0100

    wip

---
+++ smce/smcemodel.cpp
@@ -0,0 +1,6 @@
+#include "smcemodel.h"
+
+SMCEModel::SMCEModel()
+{
+
+}
---
+++ smce/smcemodel.h
@@ -0,0 +1,11 @@
+#ifndef SMCEMODEL_H
+#define SMCEMODEL_H
+
+
+class SMCEModel : public AnalysisModel
+{
+public:
+    SMCEModel();
+};
+
+#endif // SMCEMODEL_H
