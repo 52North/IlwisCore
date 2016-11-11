1f2fb74f614987ea89fbc45ca32d8752fcc3eff6
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2016-11-11 09:08:08 +0100

    eeh, should have been commited

--- smce/smcemodel.cpp
+++ smce/smcemodel.cpp
@@ -1,6 +1,25 @@
+#include "kernel.h"
+#include "ilwisdata.h"
+#include "modeller/workflow.h"
+#include "modeller/analysispattern.h"
+#include "modeller/applicationsetup.h"
+#include "modeller/model.h"
+#include "workflow/modelbuilder.h"
 #include "smcemodel.h"
+
+
+REGISTER_ANALYSISMODEL(SMCEModel,"smce")
 
 SMCEModel::SMCEModel()
 {
 
+}
+
+SMCEModel::SMCEModel(Ilwis::AnalysisPattern *p) : AnalysisModel(p){
+    _panelPath = "SMCE/SMCEPanel.qml";
+}
+
+AnalysisModel *SMCEModel::create(Ilwis::AnalysisPattern *pattern)
+{
+    return new SMCEModel(pattern);
 }
--- smce/smcemodel.h
+++ smce/smcemodel.h
@@ -1,11 +1,23 @@
 #ifndef SMCEMODEL_H
 #define SMCEMODEL_H
 
+#include <QObject>
+#include "workflow/analysismodel.h"
 
 class SMCEModel : public AnalysisModel
 {
+    Q_OBJECT
 public:
     SMCEModel();
+    SMCEModel(Ilwis::AnalysisPattern *p);
+    static AnalysisModel *create(Ilwis::AnalysisPattern *pattern);
+
+private:
+
+    NEW_ANALYSISMODEL
+
+
+
 };
 
 #endif // SMCEMODEL_H
