a012329b1891e4014889372ca9dfca1430962a42
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2016-11-15 09:16:33 +0100

    implemented name invokable

--- ilwiscoreui/models/workflow/applicationmodel.cpp
+++ ilwiscoreui/models/workflow/applicationmodel.cpp
@@ -1,6 +1,20 @@
+#include "kernel.h"
+#include "ilwisdata.h"
+#include "workflow.h"
+#include "analysispattern.h"
+#include "applicationsetup.h"
+#include "model.h"
 #include "applicationmodel.h"
 
 ApplicationModel::ApplicationModel(QObject *parent) : QObject(parent)
 {
 
+}
+
+QString ApplicationModel::name() const
+{
+    if ( _app)    {
+       return _app->name();
+    }
+    return "";
 }
--- ilwiscoreui/models/workflow/applicationmodel.h
+++ ilwiscoreui/models/workflow/applicationmodel.h
@@ -4,15 +4,23 @@
 #include <QObject>
 #include "ilwiscoreui_global.h"
 
+namespace Ilwis {
+    class ModelApplication;
+}
 class ILWISCOREUISHARED_EXPORT ApplicationModel : public QObject
 {
     Q_OBJECT
 public:
     explicit ApplicationModel(QObject *parent = 0);
 
+    QString name() const;
+
 signals:
 
 public slots:
+
+private:
+    Ilwis::ModelApplication *_app;
 };
 
 #endif // APPLICATINMODEL_H
