0f1027067ede62f8f77587b18e0d3033fd46fbf0
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2016-11-15 13:41:11 +0100

    new

---
+++ smce/smcespatialalternatives.cpp
@@ -0,0 +1,6 @@
+#include "smcespatialalternatives.h"
+
+SMCESpatialAlternatives::SMCESpatialAlternatives()
+{
+
+}
---
+++ smce/smcespatialalternatives.h
@@ -0,0 +1,11 @@
+#ifndef SMCESPATIALALTERNATIVES_H
+#define SMCESPATIALALTERNATIVES_H
+
+
+class SMCESpatialAlternatives : public ApplicationModel
+{
+public:
+    SMCESpatialAlternatives();
+};
+
+#endif // SMCESPATIALALTERNATIVES_H
