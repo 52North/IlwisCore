9acf2b6074100a926c996c0b07d29163a64cd228
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2016-11-15 13:42:30 +0100

    new

--- smce/smcespatialalternatives.cpp
+++ smce/smcespatialalternatives.cpp
@@ -1,6 +1,29 @@
+#include "kernel.h"
+#include "ilwisdata.h"
+#include "modeller/workflow.h"
+#include "modeller/analysispattern.h"
+#include "modeller/applicationsetup.h"
+#include "modeller/model.h"
+#include "workflow/modelbuilder.h"
+#include "smcemodel.h"
 #include "smcespatialalternatives.h"
 
 SMCESpatialAlternatives::SMCESpatialAlternatives()
 {
 
+}
+
+SMCESpatialAlternatives::SMCESpatialAlternatives(Ilwis::ModelApplication *app): ApplicationModel(app)
+{
+    _panels["main"] = "SMCE/SMCESpatialAlternativePanel.qml";
+}
+
+Ilwis::ModelApplication *SMCESpatialAlternatives::create(Ilwis::ModelApplication *app)
+{
+    return 0;
+}
+
+bool SMCESpatialAlternatives::execute(const QVariantMap &inputParameters, QVariantMap &outputParameters)
+{
+    return false;
 }
--- smce/smcespatialalternatives.h
+++ smce/smcespatialalternatives.h
@@ -1,11 +1,15 @@
 #ifndef SMCESPATIALALTERNATIVES_H
 #define SMCESPATIALALTERNATIVES_H
 
+#include "workflow/applicationmodel.h"
 
 class SMCESpatialAlternatives : public ApplicationModel
 {
 public:
     SMCESpatialAlternatives();
+    SMCESpatialAlternatives(Ilwis::ModelApplication *app);
+    static Ilwis::ModelApplication *create(Ilwis::ModelApplication *app);
+    Q_INVOKABLE bool execute(const QVariantMap& inputParameters, QVariantMap& outputParameters) ;
 };
 
 #endif // SMCESPATIALALTERNATIVES_H
