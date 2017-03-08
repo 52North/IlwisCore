fe6171ad2df0e34cee2360a29e5991974162c9de
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2017-03-08 09:30:45 +0100

    the displayname will be taken from the resource and not from the url

--- ilwiscoreui/models/tabmodel.cpp
+++ ilwiscoreui/models/tabmodel.cpp
@@ -1,4 +1,7 @@
 #include "tabmodel.h"
+#include "kernel.h"
+#include "mastercatalog.h"
+#include "ilwistypes.h"
 #include "datapanemodel.h"
 
 quint32 TabModel::_id = 0;
@@ -7,13 +10,23 @@ TabModel::TabModel(QObject *parent) : QObject(parent)
 
 }
 
-TabModel::TabModel(const QString &url, const QString &componentUrl, QObject *parent) :
+TabModel::TabModel(const QString &url, const QString &componentUrl,const QString& type,  QObject *parent) :
     QObject(parent),
     _componentUrl(componentUrl)
 {
-    QString path = QUrl(url).path();
-    QStringList parts = path == "" ?  url.split("/") : path.split("/");
-     _displayName = parts[parts.size() - 1];
+
+    IlwisTypes objtype = Ilwis::TypeHelper::name2type(type);
+    if ( objtype != itUNKNOWN){
+        Ilwis::Resource res = Ilwis::mastercatalog()->name2Resource(url, objtype);
+        if ( res.isValid()){
+            _displayName = res.name();
+        }
+    }
+    if ( _displayName == ""){
+        QString path = QUrl(url).path();
+        QStringList parts = path == "" ?  url.split("/") : path.split("/");
+        _displayName = parts[parts.size() - 1];
+    }
     ++_id;
 }
 
