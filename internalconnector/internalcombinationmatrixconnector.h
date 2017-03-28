18ad0d01c8c5ff7d848cd91f3cae8598af34a349
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2017-03-28 14:27:18 +0200

    new

--- internalconnector/internalcombinationmatrixconnector.h
+++ internalconnector/internalcombinationmatrixconnector.h
@@ -2,10 +2,25 @@
 #define INTERNALCOMBINATIONMATRIXCONNECTOR_H
 
 
+namespace Ilwis {
+namespace Internal {
+
+
 class InternalCombinationMatrixConnector : public IlwisObjectConnector
 {
 public:
     InternalCombinationMatrixConnector();
+
+    InternalCombinationMatrixConnector(const Resource &resource, bool load,const IOOptions& options=IOOptions());
+
+    bool loadMetaData(IlwisObject* data,const IOOptions&);
+    QString type() const;
+    virtual IlwisObject *create() const;
+    static ConnectorInterface *create(const Ilwis::Resource &resource, bool load=true,const IOOptions& options=IOOptions());
+    bool loadData(IlwisObject *, const IOOptions& options = IOOptions());
+    QString provider() const;
 };
+}
+}
 
 #endif // INTERNALCOMBINATIONMATRIXCONNECTOR_H
