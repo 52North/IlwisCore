43397a50c431cd2fb6d8310b31c826836565e6f2
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2017-04-25 12:59:51 +0200

    new

---
+++ ilwiscoreui/tableoperations/columnvisibility.cpp
@@ -0,0 +1,6 @@
+#include "columnvisibility.h"
+
+ColumnVisibility::ColumnVisibility()
+{
+
+}
---
+++ ilwiscoreui/tableoperations/columnvisibility.h
@@ -0,0 +1,11 @@
+#ifndef COLUMNVISIBILITY_H
+#define COLUMNVISIBILITY_H
+
+
+class ColumnVisibility
+{
+public:
+    ColumnVisibility();
+};
+
+#endif // COLUMNVISIBILITY_H
