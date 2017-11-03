1bd5c61dde3b2a1c3ecacffeba7f6cd785039be1
Willem Nieuwenhuis <w.nieuwenhuis@utwente.nl>
2017-11-03 13:17:51 +0100

    Definition of extent does not reverse sign of Y-coordinates anymore

--- ilwis3connector/ilwis3featureconnector.cpp
+++ ilwis3connector/ilwis3featureconnector.cpp
@@ -526,9 +526,10 @@ void FeatureConnector::writePolygon(const geos::geom::Polygon* polygon, std::ofs
 
     const std::vector<geos::geom::Coordinate>* coords = polygon->getExteriorRing()->getCoordinates()->toVector();
     writeCoords(output_file, coords);
-    output_file.write((char *)&raw,8);
+//    quint32 iraw = raw;
+    output_file.write((char *)&raw, sizeof raw);
     quint32 holeCount = polygon->getNumInteriorRing();
-    output_file.write((char *)&holeCount,4);
+    output_file.write((char *)&holeCount, sizeof holeCount);
     for(int i=0; i < holeCount; ++i ) {
         const std::vector<geos::geom::Coordinate>* coords = polygon->getInteriorRingN(i)->getCoordinates()->toVector();
         writeCoords(output_file, coords);
@@ -999,9 +1000,9 @@ bool FeatureConnector::storeMetaData(FeatureCoverage *fcov, IlwisTypes type) {
 
     _odf->setKeyValue("BaseMap","CoordBounds",QString("%1 %2 %3 %4").
                       arg(bounds.min_corner().x,0,'f',10).
-                      arg(-bounds.max_corner().y,0,'f',10).
+                      arg(bounds.max_corner().y,0,'f',10).
                       arg(bounds.max_corner().x,0,'f',10).
-                      arg(-bounds.min_corner().y,0,'f',10));
+                      arg(bounds.min_corner().y,0,'f',10));
 
     QString ext = "mpa";
     if ( hasType(type, itPOLYGON)){
